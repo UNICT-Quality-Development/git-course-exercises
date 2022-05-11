9fd15f00756535a24ab20f8f2677f09a3ef3c911
Christian <san.chri@tiscali.it>
2022-05-11 18:07:58 +0200

    first commit

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, s;
+	cout << "Insert the first number: ";
+	cin >> x;
+	cout << "Insert the second number: ";
+	cin >> y;
+	s = x+y;
+	cout << "Sum: " << s;
+	return 0;
+}
