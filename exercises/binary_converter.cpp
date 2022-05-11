565462b6bdc575b21bd3abf812021e1607feb291
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-11 20:08:51 +0200

    feat: implement binary_converter.cpp

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,40 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+
+#include<iostream>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+
+string toBinary(int num) {
+  //stringstream ss;
+  string bin;
+
+  if(num/2 == 0) {
+    bin = to_string(num%2);
+    return bin;
+  }
+
+
+  while(num>0) {
+    bin = to_string(num%2) + bin;
+    num = num/2;
+  }
+
+
+  return bin;
+}
+
+
+int main() {
+  int num;
+  
+  cout << "Insert first number: ";
+  cin >> num;
+
+  cout << "The binary number is: " << toBinary(num) << endl;
+}
