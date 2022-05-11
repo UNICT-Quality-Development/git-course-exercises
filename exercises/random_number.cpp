cb9245333e351b73123d91da5de799d8a82536ce
sergioyc40 <sergioyc40@gmail.com>
2022-05-11 18:54:30 +0200

    feat: implementazione generazione numero random tra 0 e 99

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,15 @@
   Output:
   The random number is: 4
 */
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  srand(time(0));
+  int x = rand() % 100;
+  cout << "The random number is: " << x;
+}
