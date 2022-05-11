fa052e8e0288920b63518d4d2ae70f523b682165
Lorenzo Nicotra <36ren6@gmail.com>
2022-05-11 17:51:30 +0200

    feat: aggiunto sumHomemade

---
+++ file_sumHomemade.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x, y;
+	cout << "Yo, proviamo a fare un piccolo sum\nInserisci due numeri\n>> ";
+	cin >> x;
+	cin >> y;
+	cout << "Somma: " << x+y;
+}
