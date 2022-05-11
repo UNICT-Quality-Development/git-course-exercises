57565ab72e4943339d6d764756b29314ee293820
Salvatore Randazzo <salvatore18.sr@gmail.com>
2022-05-11 17:51:26 +0200

    Sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,26 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+T sum(T a, T b) {
+    return a+b;
+}
+
+int main() {
+
+    int a, b;
+    cout << "\nInsert the first number: ";
+    cin >> a;
+
+    cout << "Insert the second number: ";
+    cin >> b;
+
+    cout << "Sum: " << sum(a, b) << endl;
+
+    return 0;
+}
