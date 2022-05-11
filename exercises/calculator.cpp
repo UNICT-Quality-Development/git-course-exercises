00d02b88111b5b905435e55097dfd6acba1508d4
Gaetano Conti <gaetanoconti01@gmail.com>
2022-05-11 18:16:48 +0200

    exercise calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,25 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a;
+int b;
+
+cout<<"Insert first number: "; 
+cin>>a;
+
+cout<<"\nInsert second number: ";
+cin>>b;
+
+cout<<"\nLa somma e': " <<a+b <<endl;
+cout<<"La differenza e': " <<a-b <<endl;
+cout<<"La moltiplicazione e': " <<a*b <<endl;
+cout<<"La Divisione e': " <<a/b <<endl;
+
+return 0;
+}
