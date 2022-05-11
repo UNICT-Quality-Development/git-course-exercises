d6ff699bed1ef63f8dadb68e6a275b0778b1cd87
Mirko Morana <m.morana094@gmail.com>
2022-05-11 18:40:08 +0200

    feat : add calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,30 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int x,y;
+
+	cout << "Insert First Number" << endl;
+	cin >> x;
+
+	cout << "Insert Second Number" << endl;
+	cin >> y;
+
+	int Sum = 0, Diff = 0, Mult = 0;
+	double Div = 0.0;
+	Sum = x + y;
+	Diff = x - y;
+	Mult = x * y;
+	if(y != 0 ){
+	Div = (double)x/(double)y;
+	cout << "Div = " << Div;
+	} else {
+	cout << "Non è possibile dividere per 0" << endl;
+	}
+	cout << "Sum = " << Sum << endl << "Diff = " << Diff << endl << "Mult = " << Mult << endl;
+}
