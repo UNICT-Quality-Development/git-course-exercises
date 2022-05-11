0f10a0711852da0f4d061048cc25d55b809cf7b0
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-11 18:10:50 +0200

    feat: implement calculator.cpp

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,42 @@
   Multiplication: 8
   Division: 2
 */
+
+
+#include<iostream>
+
+
+using namespace std;
+
+
+int main() {
+  
+  int a,b;
+
+  //Inserting first number
+  cout << "Insert first number: ";
+  cin >> a;
+  cout << endl;
+
+  //Inserting second number
+  cout << "Insert second number: ";
+  cin >> b;
+  cout << endl;
+
+  cout << "Insert first number: " << a << "\n Insert second number: " << b << endl << endl;
+
+  cout << "sum: " << a+b << endl;
+  cout << "difference: " << a-b << endl;
+  cout << "multiplication: " << a*b << endl;
+
+  if(b!=0)
+    cout << "division: " << a/b << endl;
+  else {
+    do{
+    cout << "Impossible division by zero!\n Insert new divider: ";
+    cin >> b; 
+    } while(b==0);
+
+    cout << "division: " << a/b << endl;
+  }
+}
