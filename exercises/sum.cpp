625a33b11dd9cf3e73157ea3e4e7156f14c7d079
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-11 17:44:35 +0200

    feat: complete sum exercize

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Input A"<<endl;
+    cin>>a;
+    cout<<"Input B"<<endl;
+    cin>>b;
+
+    cout<<"Sum:"<<a+b<<endl;
+
+    return 0;
+}
