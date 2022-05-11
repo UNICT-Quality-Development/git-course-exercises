aa1bb2dc4c11814a4902e576c9ddddd80ebd5ca4
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-05-11 18:34:28 +0200

    feat: implementazione verify

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -7,12 +7,32 @@ The number 3 is [not] present in the array.
 */
 
 #include <iostream>
+
+#define L 10
+
 using namespace std;
+
+bool isThere(int* arr,int val)
+{
+    //const int length=10;
+
+    for(int i=0; i<L; i++)
+        if(arr[i] == val) return true;
+    
+    return false;
+}
 
 int main()
 {
   // placeholder
-  int N[10] = [ 3, 4, 5, 1, 2, 3, 4, 9, 13, 0 ];
+    int N[L] = { 3, 4, 5, 1, 2, 3, 4, 9, 13, 0 };
+    int val;
 
-  return 0;
+    cout<<"Inserisci il valore da cercare: ";
+    cin>>val;
+
+
+    cout<<"\n\nIl numero "<<val<<(isThere(N,val) ? " e' presente" : " non e' presente");
+
+    return 0;
 }
