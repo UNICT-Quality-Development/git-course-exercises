d6b26ca422e8623914eac101febbc153087a1364
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-11 18:33:07 +0200

    chore: improvement ex1.ccp with switch case

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -11,7 +11,7 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  if (week == 1)
+  /*if (week == 1)
   {
     cout << "Monday" << endl;
   }
@@ -42,6 +42,32 @@ int main()
   else
   {
     cout << "Invalid input! Please enter week number between 1-7." << endl;
+  }*/
+
+  switch(week) {
+    case 1:
+      cout << "Monday" << endl;
+      break;
+    case 2:
+      cout << "Tuesday" << endl;
+      break;
+    case 3:
+      cout << "Wednesday" << endl;
+      break;
+    case 4:
+      cout << "Thursday" << endl;
+      break;
+    case 5:
+      cout << "Friday" << endl;
+      break;
+    case 6:
+      cout << "Saturday" << endl;
+      break;
+    case 7:
+      cout << "Sunday" << endl;
+      break;
+    default:
+      cout << "Insert a number between 1 and 7" << endl;
   }
 
   return 0;
