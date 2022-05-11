883523fd0ea1c614b0228f5f957ac0a507d1cc48
babilkebab <enzcal002@gmail.com>
2022-05-12 00:46:57 +0200

    feat: all code updated

--- exercises/ex4.cpp
+++ exercises/ex4.cpp
@@ -1,55 +1,46 @@
 /* Surprise me. */
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int search(int* months1, int* months2, int month){
+  if(month == 2) 
+    return month;
+  for(int i = 0; i < 7; i++){
+    if(months1[i] == month)
+      return 0;
+  }
+  for(int i = 0; i < 4; i++){
+    if(months2[i] == month)
+      return 1;
+  }
+  return -1;
+}
+
+
 
 int main()
 {
   int month;
+  int months1[7] = {1, 3, 5, 7, 8, 10, 12};
+  int months2[4] = {4,6,9,11};
 
   /* Input month number from user */
-  printf("Enter month number(1-12): ");
-  scanf("%d", &month);
+  cout << "Enter month number(1-12): ";
+  cin >> month;
 
-  switch (month)
-  {
-  case 1:
-    printf("31 days");
-    break;
-  case 2:
-    printf("28/29 days");
-    break;
-  case 3:
-    printf("31 days");
-    break;
-  case 4:
-    printf("30 days");
-    break;
-  case 5:
-    printf("31 days");
-    break;
-  case 6:
-    printf("30 days");
-    break;
-  case 7:
-    printf("31 days");
-    break;
-  case 8:
-    printf("31 days");
-    break;
-  case 9:
-    printf("30 days");
-    break;
-  case 10:
-    printf("31 days");
-    break;
-  case 11:
-    printf("30 days");
-    break;
-  case 12:
-    printf("31 days");
-    break;
-  default:
-    printf("Invalid input! Please enter month number between 1-12");
+  switch(search(months1, months2, month)){
+    case 0:
+      cout << "31 days"; 
+      break;
+    case 1:
+      cout << "30 days"; 
+      break;
+    case 2:
+      cout << "28/29 days"; 
+      break;
+    default:
+      cout << "Invalid input";
   }
 
   return 0;
--- exercises/risk-risiko.cpp
+++ exercises/risk-risiko.cpp
@@ -27,3 +27,52 @@
   M 3 vs 3 => blue win
   O 2 vs 1 => red win
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void bubblesort(int* arr){
+  bool flag = false;
+  while(!flag){
+    flag = true;
+    for(int i = 0; i < 2; i++){
+      if(arr[i] > arr[i+1]){
+        flag = false;
+        swap(arr[i], arr[i+1]);
+      }
+    }
+  }
+}
+
+bool risikowin(int* red, int* blue){
+  bubblesort(red);
+  bubblesort(blue);
+  int redcnt = 0;
+  for(int i = 2; i >=0; i--){
+    if(red[i] > blue[i])
+      redcnt++;
+  }
+  return redcnt > 1;
+}
+
+
+int main(){
+  srand(time(0));
+  int arrblue[3];
+  int arrred[3];
+
+  for(int i = 0; i < 3; i++){
+    arrblue[i] = (rand()%6)+1;
+    arrred[i] = (rand()%6)+1;
+  }
+
+  cout << (risikowin(arrred, arrblue) ? "BLUE WINS!" : "RED WINS!");
+  return 0;
+
+  
+
+
+}
+
