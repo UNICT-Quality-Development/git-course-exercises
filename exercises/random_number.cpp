9eaceac3689ce6f5e28cf49274e749a33b0dc95a
coccojack <grotondococco@gmail.com>
2022-05-11 21:10:04 +0200

    feat(random_number): add function for random number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,13 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+using namespace std;
+
+int genRandomNum(int range){
+  return (rand()%range)+1;
+}
+int main(){
+  srand(time(NULL));
+  cout<<"The random number is "<<genRandomNum()<<"\n";
+}
