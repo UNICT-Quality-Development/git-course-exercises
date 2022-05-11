d7c1b5ab25a3f6817a6abee234585f8925725ef8
Carmelo Seminara <carmelosem18@gmail.com>
2022-05-11 18:27:07 +0200

    feat: excercise resolution

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -8,3 +8,46 @@
     Multiplication: 8
     Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+
+double sum(double a, double b){
+    return a+b;
+}
+
+double difference(double a, double b){
+    return a-b;
+}
+
+double multiplication(double a, double b){
+    return a*b;
+}
+
+double division(double a, double b){
+    return a/b;
+}
+
+int main(){
+
+    double a, b = 0;
+
+    cout << "Insert first number: " ;
+    
+    cin>>a;
+
+    cout << "Insert second number: " ;
+
+    cin>>b;
+
+    cout<<endl;
+
+    cout<< "sum: " << sum(a,b) << endl;
+    cout<< "difference: " << difference(a,b) << endl;
+    cout<< "multiplication: " << multiplication(a,b) << endl;
+    cout<< "division: " << division(a,b) << endl;
+
+}
+
