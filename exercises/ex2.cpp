563afc5d911b474cc26b4b97d0ec961aed9edb51
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-11 18:44:41 +0200

    chore: improve ex2.cpp with array

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -5,47 +5,27 @@
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
   int week;
-
-  cout << "Enter week number(1-7): " << endl;
+  string arr[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+  cout << "Enter week number(1-7): " << endl;  
   cin >> week;
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
+  if(week>=1 && week<=7)
+    cout << arr[week-1] << endl;
+  else {
+    do {
+    cout << "Invalid input! Please enter week number between 1-7:" << endl;
+    cin >> week;
+    cout << arr[week-1] << endl;
+    }while (week < 1 || week > 7);
   }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+
+
 
   return 0;
 }
