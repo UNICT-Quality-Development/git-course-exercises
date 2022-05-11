e9c727601a468e6f6004c9b7e6ca2874dcdab7bb
Freya <frabar@giorgioscemo.com>
2022-05-11 17:59:50 +0200

    feat : add sum

---
+++ sum.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+using namespace std ; 
+
+int main(){
+
+	bool esc = true ;
+	double n , c , sum ;
+	char f ;   
+	do{
+		cout<<"Inserisci primo numero :  " ; 
+		cin>>n ;
+		cout<<endl ; 
+		cout<<"Inserisci secondo numero :  " ; 
+		cin>>c ;
+		cout<<endl ;
+		sum = n + c ; 
+		cout<<"La somma è : "<<sum <<endl; 
+		cout<<"Vuoi uscire ? premi Y per uscire N per fare un'altra somma ";
+		cin<<f; 
+		if(f=='Y') esc = false ;
+		else esc = true ;    
+	}while(esc) ; 
+}
