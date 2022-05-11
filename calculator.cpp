1e1974d2c59363a1891a996c9e9643fa52660acc
Denise Russo <x81000632@studium.unict.it>
2022-05-11 18:50:35 +0200

    feat: implement calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num1, num2;
+
+   cout << "Insert first number: ";
+   cin >> num1;
+
+   cout << "Insert second number: ";
+   cin >> num2;
+   cout << endl;
+
+   cout << "Sum: " << num1 + num2 << endl;
+   cout << "Difference: " << num1 - num2 << endl;
+   cout << "Multiplication: " << num1 * num2 << endl;
+   cout << "Division: " << num1 / num2 << endl;
+}
