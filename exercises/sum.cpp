940c37e293b433e6aa2476979a73975cdffbe106
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-11 17:44:36 +0200

    feat: implements sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+int main() {
+  int first, second;
+  
+  std::cout << "Insert the first number: ";
+  std::cin >> first;
+  std::cout << "Insert the second number: ";
+  std::cin >> second;
+  
+  std::cout << "Sum: " << first+second << std::endl;
+
+  return 0;
+}
