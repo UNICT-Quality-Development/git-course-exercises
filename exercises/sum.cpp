4cff1ae90a675e55c409d4d62ee7c21ae6ca30cf
Stefano Romeo <stefano.romeo01@hotmail.com>
2022-05-11 18:25:53 +0200

    feat: added sum code

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int a, b;
+
+	cout << "Inserisci il primo numero : " << endl;
+	cin >> a;
+	cout << "Inserisci il secondo numero : " << endl;
+	cin >> b;
+
+	cout << a+b;
+
+}
