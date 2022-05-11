616622a08cf552d84244a1871b4d6f8e4ff92f36
Mirko Morana <m.morana094@gmail.com>
2022-05-11 18:24:14 +0200

    feat : add random number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,12 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	int num = rand()%10;
+	cout << "The random number is : " << num << endl;
+	}
