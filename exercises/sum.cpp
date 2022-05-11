46ef38960fb7738497a8d7fa14f999e03c6f747e
PowerMagnum <salvopollicina2002@gmail.com>
2022-05-11 17:51:55 +0200

    feat(sum.cpp): Implemented sum program

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,20 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+double sum(double n1, double n2){
+	return n1 + n2;
+}
+
+int main(){
+	double n1, n2;
+	cout<<"Insert the first number: ";
+	cin>>n1;
+	cout<<"Insert the second number: ";
+	cin>>n2;
+	cout<<"Sum: "<<sum(n1,n2);
+	return 0;
+}
