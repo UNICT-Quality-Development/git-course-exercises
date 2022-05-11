3e3535c04b1f4c7935b62effe928a310dd1e84fc
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-11 17:58:16 +0200

    feat: implement random_number.cpp

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,24 @@
   Output:
   The random number is: 4
 */
+
+
+
+#include<iostream>
+#include<cstdlib>
+#include<climits>
+#include<ctime>
+
+using namespace std;
+
+
+int main() {
+  
+  srand(time(0));
+
+  //Generating the random number
+  int random_num = rand()%RAND_MAX;
+
+  cout << "The random number is: " << random_num << endl;
+
+}
