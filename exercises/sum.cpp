d08612f2c479cb2d507152a5ccff7c67689bf919
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-05-11 18:03:37 +0200

    feat: implementazione somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,27 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+void input(int& x,int& y)
+{
+    cout<<"Inserisci il primo numero: ";
+    cin>>x;
+
+    cout<<"Inserisci il secondo numero: ";
+    cin>>y;
+}
+
+int somma(const int x,const int y) { return x+y; }
+
+int main()
+{
+    int x,y;
+
+    input(x,y);
+    cout<<x<<"+"<<y<<"="<<somma(x,y);
+
+}
