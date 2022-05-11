654928457eeaece58ef94546024f79473cb2a35f
Massimo Belfiore <massimobelfiorechitarra@gmail.com>
2022-05-11 18:04:32 +0200

    definita somma tra 2 valori

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int , int );
+
+int main(){
+    int x, y;
+    cout << "inserisci il primo numero intero";
+    cin >> x;
+    cout << "inserisci il secondo numero intero";
+    cin >> y;
+    cout <<"la somma di due valori è: " << sum(x, y) << endl;
+}
+
+int sum(int x, int y){
+    return x+y;
+} 
