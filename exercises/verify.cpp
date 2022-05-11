8c73532b69119af5c0485f682b7d2a1db70b9f2b
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-11 17:55:51 +0200

    feat: now verifies if a number is present in a pre-defined array

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -12,5 +12,21 @@ using namespace std;
 int main() {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int verify;
+  bool isPresent = false;
+
+  cout << "Insert number: ";
+  cin >> verify;
+  
+  for (int i=0; i<10 && !isPresent; i++) {
+    isPresent = N[i] == verify;
+  }
+
+  cout << "The number " << verify << " is";
+  if (!isPresent) {
+    cout << " not"; 
+  }
+  cout << " present in the array.\n";
+
   return 0;
 }
