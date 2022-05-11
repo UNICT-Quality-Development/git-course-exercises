1eeea879f8628bc703c27bf6acfdfda1a1593226
angelo <angelodemarco2015@gmail.com>
2022-05-11 17:56:43 +0200

    sum.cpp: implementation

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,22 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+ int a,b;
+
+ cout<<"Inserisci il primo valore: ";
+ cin>>a;
+ 
+ cout<<"Inserisci il secondo valore: ";
+ cin>>b;
+
+ cout<<"Somma: " << (a+b);
+
+
+ return 0;
+}
