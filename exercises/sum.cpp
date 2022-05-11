7db898f07224750726a7f6989dc7ac2a01823efa
Vito Giordano <vitogiordano00.vg@gmail.com>
2022-05-11 18:25:42 +0200

    feat: Sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,20 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int a, b;
+
+cout << "Insert First number: ";
+cin >> a;
+cout << "Insert Second Number: ";
+cin >> b;
+int Sum = a+b;
+cout << "Sum: " << Sum;
+
+}
