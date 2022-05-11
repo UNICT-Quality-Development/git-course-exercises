d5a35694f3df611e16ae0cadf770247e66eccc1b
coccojack <grotondococco@gmail.com>
2022-05-11 17:45:36 +0200

    feat(sum): add sum algorithm

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+int main(){
+  int num1,num2,sum;
+  num1=num2=0;
+  cout<<"Insert the first number: ";
+  cin>>num1;
+  cout<<"Insert the second number: ";
+  cin>>num2;
+  sum=num1+num2;
+  cout<<"Sum:"<<sum<<"\n";
+  return 0;
+}
