974784d12646c50b09c78a9257cf1770fc5bf557
Mirko Morana <m.morana094@gmail.com>
2022-05-12 19:37:48 +0200

    feat : add Binary Converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,36 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+
+using namespace std;
+
+string Binary(int val){
+  string Bin = "";
+  while(val>0){
+    if(val%2 == 0){
+        Bin += '0';
+    } else {
+        Bin += '1';
+    }
+    val/=2;
+  }
+  return Bin;
+}
+
+int get(){
+  int x;
+  cout << ", InsertValue" << endl;
+  cin >> x;
+  return x;
+}
+
+int main(){
+
+cout << "\nHello";
+
+int y = get();
+
+cout << "\nThe Binary Value is : " << Binary(y) << endl;
+
+}
