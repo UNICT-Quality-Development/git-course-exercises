b3b42aa1bc2d1ed72ae2ffd59ce6534813c6fed4
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-12 12:44:04 +0200

    chore: improve readability

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -8,24 +8,21 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-  int week;
-  string arr[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-  cout << "Enter week number(1-7): " << endl;  
-  cin >> week;
+int main() {
+	int week;
+	string arr[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	cout << "Enter week number(1-7): " << endl;
+	cin >> week;
 
-  if(week>=1 && week<=7)
-    cout << arr[week-1] << endl;
-  else {
-    do {
-    cout << "Invalid input! Please enter week number between 1-7:" << endl;
-    cin >> week;
-    cout << arr[week-1] << endl;
-    }while (week < 1 || week > 7);
-  }
+	if (week >= 1 && week <= 7)
+		cout << arr[week - 1] << endl;
+	else {
+		do {
+			cout << "Invalid input! Please enter week number between 1-7:" << endl;
+			cin >> week;
+			cout << arr[week - 1] << endl;
+		} while (week < 1 || week > 7);
+	}
 
-
-
-  return 0;
+	return 0;
 }
