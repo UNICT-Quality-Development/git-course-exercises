a113829d785f31ec59ae52bd0441f378613cedac
coccojack <grotondococco@gmail.com>
2022-05-12 12:57:23 +0200

    add space in code

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -7,11 +7,13 @@
 #include <iostream>
 using namespace std;
 
-int genRandomNum(int range){
-  return (rand()%range)+1;
+int genRandomNum(int range)
+{
+  return (rand() % range) + 1;
 }
-int main(){
+int main()
+{
   srand(time(NULL));
-  cout<<"The random number is "<<genRandomNum(10)<<"\n";
+  cout << "The random number is " << genRandomNum(10) << "\n";
   return 0;
 }
