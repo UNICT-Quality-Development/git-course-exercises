cdbf981c1fdb1480a8cccb5f63f7a3be31f46210
Angelo Iacona <iaconaangelo.ai@gmail.com>
2022-05-12 21:12:13 +0200

    feat: implement random_number.cpp

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,18 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  srand(time(NULL));
+
+  int number = rand();
+  cout << "The random number is: " << number << endl;
+
+  return 0;
+}
