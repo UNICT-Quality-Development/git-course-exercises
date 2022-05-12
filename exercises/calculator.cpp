221ec4e297ce59fce233792a7dc1e22cab9e5432
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-12 10:05:43 +0200

    chore: improve tabulation of code

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -16,39 +16,39 @@
 
 using namespace std;
 
-void insertNumber(int& a) {
-  cout << "Insert number: ";
-  cin >> a;
-  cout << endl;
+void insertNumber(int &a) {
+	cout << "Insert number: ";
+  	cin >> a;
+  	cout << endl;
 }
 
-
 int main() {
-  
-  int a, b;
 
-  //Inserting first number
-  insertNumber(a);
+  	int a, b;
 
-  //Inserting second number
-  insertNumber(b);
+  	// Inserting first number
+  	insertNumber(a);
 
-  cout << "Insert first number: " << a << "\n Insert second number: " << b << endl << endl;
+  	// Inserting second number
+  	insertNumber(b);
 
-  cout << "sum: " << a+b << endl;
-  cout << "difference: " << a-b << endl;
-  cout << "multiplication: " << a*b << endl;
+  	cout << "Insert first number: " << a << "\n Insert second number: " << b << endl << endl;
 
-  if (b!=0) {
-    cout << "division: " << a/b << endl;
-  } else {
-    do{
-    cout << "Impossible division by zero!\n Insert new divider: ";
-    cin >> b; 
-    } while(b==0);
+  	cout << "sum: " << a + b << endl;
+  	cout << "difference: " << a - b << endl;
+  	cout << "multiplication: " << a * b << endl;
 
-    cout << "division: " << a/b << endl;
-  }
+  	if (b!=0) {
+    	cout << "division: " << a / b << endl;
+  	} else {
+    	
+		do {
+      		cout << "Impossible division by zero!\n Insert new divider: ";
+      		cin >> b;
+    	}while (b == 0);
 
-  return 0;
+    	cout << "division: " << a / b << endl;
+  	}
+
+  	return 0;
 }
