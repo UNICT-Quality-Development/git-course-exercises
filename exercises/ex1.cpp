6f7a764cbee8476ab037bcbf84051959b1aee97c
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-13 19:45:54 +0200

    refactor: improve readability

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -3,42 +3,40 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int week;
+int main() {
+	int week;
 
-  /* Input week number from user */
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+	/* Input week number from user */
+	cout << "Enter week number(1-7): " << endl;
+	cin >> week;
 
+	switch (week) {
+	case 1:
+		cout << "Monday";
+		break;
+	case 2:
+		cout << "Tuesday";
+		break;
+	case 3:
+		cout << "Wednesday";
+		break;
+	case 4:
+		cout << "Thursday";
+		break;
+	case 5:
+		cout << "Friday";
+		break;
+	case 6:
+		cout << "Saturday";
+		break;
+	case 7:
+		cout << "Sunday";
+		break;
+	default:
+		cout << "Insert a number between 1 and 7";
+	}
 
-  switch(week) {
-    case 1:
-      cout << "Monday";
-      break;
-    case 2:
-      cout << "Tuesday";
-      break;
-    case 3:
-      cout << "Wednesday";
-      break;
-    case 4:
-      cout << "Thursday";
-      break;
-    case 5:
-      cout << "Friday";
-      break;
-    case 6:
-      cout << "Saturday";
-      break;
-    case 7:
-      cout << "Sunday";
-      break;
-    default:
-      cout << "Insert a number between 1 and 7";
-  }
+	cout << endl;
 
-  cout << endl;
-
-  return 0;
+	return 0;
 }
