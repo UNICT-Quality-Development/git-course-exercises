086cec6115df26901034cc42ca1ddb58dc77fd7a
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2022-05-13 19:44:55 +0200

    chore: removing the endl in the switch case

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -14,29 +14,31 @@ int main()
 
   switch(week) {
     case 1:
-      cout << "Monday" << endl;
+      cout << "Monday";
       break;
     case 2:
-      cout << "Tuesday" << endl;
+      cout << "Tuesday";
       break;
     case 3:
-      cout << "Wednesday" << endl;
+      cout << "Wednesday";
       break;
     case 4:
-      cout << "Thursday" << endl;
+      cout << "Thursday";
       break;
     case 5:
-      cout << "Friday" << endl;
+      cout << "Friday";
       break;
     case 6:
-      cout << "Saturday" << endl;
+      cout << "Saturday";
       break;
     case 7:
-      cout << "Sunday" << endl;
+      cout << "Sunday";
       break;
     default:
-      cout << "Insert a number between 1 and 7" << endl;
+      cout << "Insert a number between 1 and 7";
   }
+
+  cout << endl;
 
   return 0;
 }
