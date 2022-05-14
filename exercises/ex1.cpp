8db931ca426a2ba0b028ad5e4f511e77419fac1d
Nemoraffa <rflspano@gmail.com>
2022-05-14 02:08:30 +0200

    feat: implementing ex1

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -1,48 +1,66 @@
 /* Improve this program using a switch-case. */
 
 #include <iostream>
+#include <limits>
 using namespace std;
+
+template <class Type>
+Type insert_and_control(Type data) //cin control
+{
+	do
+	{
+		cin >> data;
+		if (cin.fail())
+		{
+			cerr << "Insert a number!" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		else
+			break;
+	} while (true);
+	return data;
+}
 
 int main()
 {
-  int week;
+	int week;
 
-  /* Input week number from user */
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+	/* Input week number from user */
+	cout << "Enter week number(1-7): " << endl;
+	do
+	{
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+		week = insert_and_control(week); 
+		switch (week)
+		{
+		case 1:
+			cout << "Monday" << endl;
+			break;
+		case 2:
+			cout << "Tuesday" << endl;
+			break;
+		case 3:
+			cout << "Wednesday" << endl;
+			break;
+		case 4:
+			cout << "Thursday" << endl;
+			break;
+		case 5:
+			cout << "Friday" << endl;
+			break;
+		case 6:
+			cout << "Saturday" << endl;
+			break;
+		case 7:
+			cout << "Sunday" << endl;
+			break;
+		default:
+			cerr << "Invalid input! Please enter week number 1-7.\n"<< endl;
+			continue;
+		}
+		break;
+	} while (true);
 
-  return 0;
+	return 0;
 }
