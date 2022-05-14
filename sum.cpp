6439f14a63f4ddd1b3029f1a21184be36cf46a18
Massimo Belfiore <massimobelfiorechitarra@gmail.com>
2022-05-14 10:12:17 +0200

    refactor: migliorata ridondanza del codice

--- sum.cpp
+++ sum.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 
 int sum(int , int );
+int numberOfInput();
 
 int main(){
-    int x, y;
-    cout << "inserisci il primo numero intero";
-    cin >> x;
-    cout << "inserisci il secondo numero intero";
-    cin >> y;
+
+    int x = numberOfInput();
+    int y = numberOfInput();
+
     cout <<"la somma di due valori è: " << sum(x, y) <<endl;
+    return 0;
 }
 
 int sum(int x, int y){
     return x+y;
-} 
+}
+
+int numberOfInput(){
+    int num;
+    cout << "inserisci un numero: ";
+    cin >> num;
+    cout << endl;
+    cout <<"numero inserito: " << num << endl;
+    return num;
+}
