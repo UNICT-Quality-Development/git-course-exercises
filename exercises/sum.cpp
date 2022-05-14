28aedd4d5f23269bc1ccb48ced09bf83ac0d707e
Nemoraffa <rflspano@gmail.com>
2022-05-14 02:22:50 +0200

    feat: implementing sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,41 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+template <class Type>
+Type sum(Type const first, Type const second)
+{
+  return first + second;
+}
+
+template <class Type>
+Type insert_and_control(Type data) //cin control
+{
+  do
+  {
+    cin >> data;
+    if (cin.fail())
+    {
+      cerr << "Insert a number!" << endl;
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    else
+      break;
+  } while (true);
+  return data;
+}
+
+int main()
+{
+  double first, second;
+  cout << "Insert the first number: ";
+  first = insert_and_control(first);
+  cout << "Insert the second number: ";
+  second = insert_and_control(second);
+  cout << "Sum: " << sum<double>(first, second);
+}
