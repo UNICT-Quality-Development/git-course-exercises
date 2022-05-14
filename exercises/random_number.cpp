009f552cb76750a15f916217957a18c6fa0b2ffb
Nemoraffa <rflspano@gmail.com>
2022-05-14 02:16:03 +0200

    feat: implementing random_number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,13 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void)
+{
+  srand(time(0));
+  cout << "The random number is: " << rand();
+}
