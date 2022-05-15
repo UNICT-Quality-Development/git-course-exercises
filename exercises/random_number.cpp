f8107fe905598a4af6bdc99a7c0c2a48a82e48a7
Salvatore Randazzo <salvatore18.sr@gmail.com>
2022-05-15 18:33:41 +0200

    Add random algorithm

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,24 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void randomGenerator(int& n) {
+    n = rand()%100+1;
+}
+
+int main() {
+
+    srand(time(0));
+
+    int a;
+    randomGenerator(a);
+
+    cout << "The random number is: " << a << endl;
+
+    return 0;
+}
