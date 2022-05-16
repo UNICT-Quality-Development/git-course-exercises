a9cdcf34680d492478084e8357188827a04eab0f
Matteovullo <matteovullo61@gmail.com>
2022-05-16 17:29:57 +0200

    feat: description

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,36 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>sum(T a, T b){
+	return a+b;
+}
+
+template<typename T>difference(T a, T b){
+	return a-b;
+}
+
+template<typename T>multiplication(T a, T b){
+	return a*b;
+}
+
+template<typename T>division(T a, T b){
+	if(b != 0){
+		return a/b;
+	}else{
+		return -1;
+	}
+}
+
+int main(){
+	cout << sum(4, 2) << endl;
+	cout << difference(4, 2) << endl;
+	cout << multiplication(4, 2)<< endl;
+	cout << division(4, 2) << endl;
+	
+	return 0;
+}
