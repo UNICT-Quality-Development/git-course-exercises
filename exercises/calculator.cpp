de9ee5938f7bd5055d48130d346a2055528426f9
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-05-17 17:18:53 +0200

    feat: implementing calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,45 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+double getNumber(double& n){
+      std::cout << "Insert a number: ";
+      std::cin >> n;  
+} 
+
+double calculate(double n1, double n2, char op){
+        switch(op){
+              case 'S':  return n1+n2;
+                         break;
+
+              case 'D':  return n1-n2;
+                         break;
+
+              case 'M':  return n1*n2;
+                         break;
+
+              case 'd':  if(n2 == 0)
+                          std::cerr << "ERROR, IMPOSSIBLE TO DIVIDE BY ZERO!" << "\n\n";
+                         else
+                          return n1/n2;
+                         break;  
+
+              default: std::cerr << "ERROR, OPERATION NOT EXIST!" << std::endl << std::endl;
+        }
+}
+
+int main(){
+   double n1,n2;
+   getNumber(n1);
+   getNumber(n2);
+
+   std::cout << std::endl;
+   
+   std::cout << "Sum: " << calculate(n1,n2,'S') << std::endl;
+   std::cout << "Difference: " << calculate(n1,n2,'D') << std::endl;
+   std::cout << "Multiplication: " << calculate(n1,n2,'M') << std::endl;
+   std::cout << "Division: " << calculate(n1,n2,'d') << std::endl;
+
+}
