1357ddf1998a24365d7117b126b042677328078e
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-05-17 16:46:05 +0200

    feat: implement converting a number into his binary form

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,33 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+
+std::string convert(int n){
+      std::string res = "";
+      while(n != 0){
+         if(n % 2 == 0)
+           res += "0";
+         else 
+           res += "1";
+         
+         n /= 2;
+      }
+      
+      std::string resInvert = "";
+      for(int i = res.length()-1; i >= 0; i--)
+        resInvert += res[i];
+      
+      return resInvert;
+}
+
+int main(){
+    
+    int num;
+
+    std::cout << "Insert first number: ";
+    std::cin >> num;
+
+    std::cout << "The binary number is: " << convert(num) << std::endl;
+}
