78f0e09ef6311c6ccdd459c7b23dc51605eefdf6
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-18 15:15:16 +0200

    chore: move endl after switch

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -14,31 +14,32 @@ int main()
   switch (week)
   {
   case 1:
-    cout << "Monday" << endl;
+    cout << "Monday";
     break;
   case 2:
-    cout << "Tuesday" << endl;
+    cout << "Tuesday";
     break;
   case 3:
-    cout << "Wednesday" << endl;
+    cout << "Wednesday";
     break;
   case 4:
-    cout << "Thursday" << endl;
+    cout << "Thursday";
     break;
   case 5:
-    cout << "Friday" << endl;
+    cout << "Friday";
     break;
   case 6:
-    cout << "Saturday" << endl;
+    cout << "Saturday";
     break;
   case 7:
-    cout << "Sunday" << endl;
+    cout << "Sunday";
     break;
   
   default:
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
+    cout << "Invalid input! Please enter week number between 1-7.";
     break;
   }
+  cout << endl;
 
   return 0;
 }
