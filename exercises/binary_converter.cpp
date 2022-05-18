2dbe3f966868a95e8b01268a412c2fa48aa49c53
Stefano Borzi <Stefano@example.com>
2022-05-18 16:49:26 +0200

    feat: end binary converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,22 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string num_tmp;
+    int n, lenght;
+    std::cin >> n;
+
+    while(n != 0){
+        if(n%2 == 0){
+            num_tmp += '0';
+        }else {
+            num_tmp += '1';
+        }
+        n = n/2;
+    }
+    std::string num_bin(num_tmp.rbegin(), num_tmp.rend());
+    std::cout << "Numero binario: " << num_bin;
+}
