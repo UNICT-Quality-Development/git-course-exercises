65540f176d056dc9487bbc263341b5ef13b7ae93
Angelo Iacona <iaconaangelo.ai@gmail.com>
2022-05-19 13:00:00 +0200

    refactor: change camelcase for function naming and refactor of input

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -7,20 +7,18 @@
 #include <iostream>
 using namespace std;
 
-int input()
+void input(int &weekNumber)
 {
-  int week_number;
   cout << "Enter week number(1-7): ";
-  cin >> week_number;
-  return week_number;
+  cin >> weekNumber;
 }
 
-void print_day(string week[], int week_day)
+void printDay(string week[], int weekDay)
 {
   string day;
-  if (week_day >= 1 && week_day <= 7)
+  if (weekDay >= 1 && weekDay <= 7)
   {
-    day = week[week_day];
+    day = week[weekDay];
   }
   else
   {
@@ -32,9 +30,10 @@ void print_day(string week[], int week_day)
 
 int main()
 {
-  int week_number = input();
+  int weekNumber;
+  input(weekNumber);
   string week[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-  print_day(week, week_number);
+  printDay(week, weekNumber);
   return 0;
 }
