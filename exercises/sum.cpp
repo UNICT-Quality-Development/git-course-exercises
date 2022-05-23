d5bd5e99f9ef44eb63f148264ab60d278d373a1f
Giorgio Guardo <giorgioguardo@hotmail.com>
2022-05-24 01:39:54 +0200

    feat(sum.cpp): add sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,27 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {return a + b;}
+
+bool toRepeat(char ch){
+  if(ch=='y') return true;
+  return false;
+}
+
+int main(){
+  char choise='n';
+  int n,m;
+  do{
+    cout << "Insert the first number: ";
+    cin >> n;
+    cout << "inserisci il secondo addendo: ";
+    cin >> m;
+    cout << "Sum: " << sum(n,m) << "\nDo you want another sum? (y/n)\n";
+    cin >> choise;
+    cout << endl;
+  }while(toRepeat(choise));
+  return 0;
+}
