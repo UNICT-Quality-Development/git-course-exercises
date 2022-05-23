b14f9313c57e02716f672e74a72395c445d27b0d
carlospata <101207207+carlospata@users.noreply.github.com>
2022-05-23 14:54:59 +0200

    Update exercises/ex4.cpp
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- exercises/ex4.cpp
+++ exercises/ex4.cpp
@@ -11,7 +11,13 @@ int main()
   scanf("%d", &month);
 
   switch(month){
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12: 
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: 
          printf("31");
          break;
         
