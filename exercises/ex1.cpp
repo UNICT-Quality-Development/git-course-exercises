621a96e419fe7dcac0fb5abf1d38e8ef9c7dfe6a
Giorgio Guardo <giorgioguardo@hotmail.com>
2022-05-24 10:40:27 +0200

    refactor: delete endl redundancy

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -11,38 +11,39 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  switch (){
+  switch(week){
     case 1:
-      cout << "Monday" << endl;
+      cout << "Monday";
       break;
     
     case 2:
-      cout << "Tuesday" << endl;
+      cout << "Tuesday";
       break;
     
     case 3:
-      cout << "Wednesday" << endl;
+      cout << "Wednesday";
       break;
     
     case 4:
-      cout << "Thursday" << endl;
+      cout << "Thursday";
       break;
 
     case 5:
-      cout << "Friday" << endl;
+      cout << "Friday";
       break;
 
     case 6:
-      cout << "Saturday" << endl;
+      cout << "Saturday";
       break;
 
     case 7:
-      cout << "Sunday" << endl;
+      cout << "Sunday";
       break;
 
     default:
-      cout << "Invalid input! Please enter week number between 1-7." << endl;
+      cout << "Invalid input! Please enter week number between 1-7.";
   }
+  cout << endl;
 
   return 0;
 }
