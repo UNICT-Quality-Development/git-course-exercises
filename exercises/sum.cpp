f9e90b937a192a442878cbe7dd1a5548c804a80a
weelics <weelixhill@gmail.com>
2022-10-19 17:27:50 +0200

    feat: add sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,22 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+
+using namespace std;
+
+int sum(int a , int b){
+  return a + b;
+}
+
+int main(){
+  int a,b;
+
+  cout<<"Insert the first number: ";
+  cin >> a;
+  cout<<"Insert the second number: ";
+  cin >> b;
+  cout<<"Sum: "<< sum(a,b) <<endl;
+}
+
+
