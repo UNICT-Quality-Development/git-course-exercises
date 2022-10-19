05bb0b447160ee077a1ddf664bae036d5543f5fd
Real96 <chicco.gaeta@gmail.com>
2022-10-19 18:31:24 +0200

    feat: modified random_number.cpp

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,17 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main() {
+  srand(time(NULL));
+
+  cout << "The random number is: " << (rand() % 10);
+
+  return 0;
+}
