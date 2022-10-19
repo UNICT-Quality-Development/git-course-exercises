e4fe75edeb99a38010b7beb1a6723c638e2b3a35
Helias <stefanoborzi32@gmail.com>
2022-10-19 17:13:54 +0200

    implement sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int main() {
+
+  std::cout << "sum" << sum(3,4) << std::endl;
+
+  return 0;
+}
