a229e5b08f9967de60d4e61af60a17e0298a914a
Salvo <salvoscavo49@gmail.com>
2022-10-19 18:54:50 +0200

    feat: random number in range

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,32 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+#define EXIT_SUCCECS 0
+
+int generateRandom(int max,int min)
+{
+  return (rand() % max - min + 1 ) + min;
+}
+
+int main()
+{
+  int max,min,x;
+  do 
+  {
+    cout<<"Insert the higher number that can be generated: ";
+    cin>>max;
+    cout<<"Insert the lowe number that can be generated: ";
+    cin>>min;
+  }while (max<=min);
+
+  x=generateRandom(max,min);
+
+  cout<<"The random number is: "<<x<<endl;  
+  
+  return EXIT_SUCCESS;
+}
