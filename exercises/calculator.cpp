22c8a5036fa9891bfa43bf7dbc1a03dd2ff13c5f
Real96 <chicco.gaeta@gmail.com>
2022-10-19 18:17:59 +0200

    feat: adedd calculator functions

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,40 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+double sum(double x, double y) {
+  return x + y;
+}
+
+double sub(double x, double y) {
+  return x - y;
+}
+
+double mul(double x, double y) {
+  return x * y;
+}
+
+double div(double x, double y) {
+  return x / y;
+}
+
+int main() {
+  double x, y;
+
+  cout << "Insert the first number: ";
+  cin >> x;
+
+  cout << "Insert the second number: ";
+  cin >> y;
+
+  cout << "SUM: " << sum(x, y) << endl;
+  cout << "Difference: " << sub(x, y) << endl;
+  cout << "Multiplication: " << mul(x, y) << endl;
+  cout << "Division: " << div(x, y) << endl;
+
+  return 0;
+}
