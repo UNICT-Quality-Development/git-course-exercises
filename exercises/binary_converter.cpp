343d902c30c83e491ec6153ca13c285abc852bd6
RobyMark <rob_marchese@virgilio.it>
2022-10-19 18:24:04 +0200

    feat: Implemented binary_converter.cpp

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -1,7 +1,34 @@
-/*
-  Write a program that given a number as input convert it in binary.
+#include <iostream>
+#include <string>
 
-  Output:
-  Insert first number: 8
-  The binary number is: 1000
-*/
+using namespace std;
+
+string convert(int n);
+
+int main(){
+	int n;
+	cout<<"Insert number to convert:\n";
+	cin>>n;
+	if(n>=0){
+		cout<<"In binary: "<<convert(n)<<"\n";
+	}
+	else{
+		cout<<"Must be positive!\n";
+	}
+}
+
+
+string convert(int n){
+	string s="";
+	do{
+		int x=n%2;
+		if(x==1){
+			s="1"+s;
+		}
+		else{
+			s="0"+s;
+		}
+		n=n>>1;
+	} while(n>0);
+	return s;
+}
