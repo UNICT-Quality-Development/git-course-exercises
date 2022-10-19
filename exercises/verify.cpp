9772a165b24b837696c10b9d4a949ed14a7e99fc
weelics <weelixhill@gmail.com>
2022-10-19 18:20:43 +0200

    feat: function verify

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -9,10 +9,25 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
+bool verify(int *N,int input){
+ for (int i = 0; i < 10; i++)
+  {
+    if(N[i] == input){
+      return true;
+    }
+  }
+
+  return false;
+}
+
 int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int input;
+  cin >> input;
+  if(verify(N,input)) cout<<"the number "<<input<<" is present in array"<<endl;
+  else cout<<"the number "<<input<<" isn't present in array"<<endl;
 
   return 0;
 }
