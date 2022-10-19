f143aa4bbbe6e7520cefd5ddc4382b6e63e32f5a
Davide496 <milazzodavidesc@gmail.com>
2022-10-19 16:54:59 +0200

    feat: implemented array search

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -7,12 +7,31 @@ The number 3 is [not] present in the array.
 */
 
 #include <iostream>
+
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR -1
+
 using namespace std;
 
 int main()
 {
-  // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
-  return 0;
+  int dim = 10;
+  int number;
+
+  cout << "What number are you searching for?\n> ";
+  cin >> number;
+
+  // placeholder
+  int N[dim] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+
+  for (int i=0; i<dim; i++) {
+    if (N[i] == number) {
+      cout << "The number " << number << " is present in the array" << endl;
+      return EXIT_SUCCESS;
+    }
+  }
+
+  cout << "The number " << number << " is not present in the array" << endl;
+  return EXIT_ERROR;
 }
