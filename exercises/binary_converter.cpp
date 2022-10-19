c1baf0e3555acf9942dc8b342b2f4ba2fa34e320
Gianluigi Tozzolino <gianluigi.tozzolino@herakel.com>
2022-10-19 18:19:51 +0200

    feat: add binary converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,36 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#define MAX_BITS 32
+#include <stdio.h>
+#include <limits.h>
+
+bool bits[MAX_BITS];
+
+int main()
+{
+  int N;
+  printf("Please enter a positive number below: ");
+  scanf("%d", &N);
+
+  if (N <= 0)
+  {
+    printf("Please enter a positive number below %d\n", INT_MAX);
+    return 1;
+  }
+
+  int j = MAX_BITS - 1;
+
+  while (N > 0)
+  {
+    bits[j--] = N % 2;
+    N >>= 1;
+  }
+
+  for (int i = 0; i < MAX_BITS; i++)
+    printf("%d", bits[i]);
+  printf("\n");
+
+  return 0;
+}
