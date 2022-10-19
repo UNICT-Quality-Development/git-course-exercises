0ba29c700fee41bfd6482d1fdcdc9402f55f9927
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-10-19 18:52:53 +0200

    feat: implemented sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <stdio.h>
+
+int x, y;
+
+int main(){
+	printf("Insert the first number: ");
+	scanf("%d", &x);
+
+	printf("Insert the second number: ");
+	scanf("%d", &y);
+
+	printf("Sum: %d", x + y);
+	return 0;
+}
