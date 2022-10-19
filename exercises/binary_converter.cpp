9abb46624d3757c363b63c19eb4b2f521872aca8
Celeste2255 <giuliamaugeri.gm@gmail.com>
2022-10-19 18:53:29 +0200

    feat: binary convertion implemented

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,35 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define EXIT_SUCCESS 0
+
+int main(){
+
+int n;
+string b;
+b = " ";
+cout << "Inserisci un numero: ";
+cin >> n ;
+
+while (n > 0) {
+  if (n%2 = 0)
+     b = '0' + b;
+  else 
+     b = '1' + b;
+
+  n = n/2 ;   
+}
+
+cout << "Numero convertito in binario: " << b ;
+
+return EXIT_SUCCESS;
+
+}
+
+
+
+
