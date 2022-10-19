7fb2608e644b02d865e88fa3ef85c7d6e15af36e
Mary <cioacmaria@gmail.com>
2022-10-19 20:24:36 +0200

    file sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,30 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream> 
+using namespace std; 
+
+int somma (int x, int y){
+	return x+y; 
+}
+
+int Inserire(){
+	int x; 
+	
+	cin>>x; 
+	
+	return x;  
+}
+
+
+int main(){
+	int a, b; 
+	
+	cout<<"inserisci due numeri: "; 
+	a=Inserire(); 
+	b=Inserire(); 
+	
+	cout<<"la somma tra due numeri e'': " <<somma(a,b); 
+	
+}
