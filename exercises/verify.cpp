f8ddb8b555b21865493badbcab40a8b7a47b8658
Andrei Stefan <andry4you@hotmail.it>
2022-10-19 18:20:47 +0200

    feat: implemented verify

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -7,6 +7,8 @@ The number 3 is [not] present in the array.
 */
 
 #include <iostream>
+#define EXIT_SUCCESS 0
+
 using namespace std;
 
 int main()
@@ -14,5 +16,25 @@ int main()
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
-  return 0;
+  int n;
+  bool check = false;
+
+  cout << "Inserire il numero da cercare all'interno dell'array: ";
+  cin >> n;
+
+  cout << "L'array predefinito e': [";
+
+  for (int i=0; i<10; i++){
+    if(N[i] == n)
+      check=true;
+    
+    cout << N[i] << " ";
+  }
+
+  if(check)
+    cout << "]\n Il numero inserito e' presente nell'array.";
+  else
+    cout << "]\n Il numero inserito non e' presente nell'array.";
+
+  return EXIT_SUCCESS;
 }
