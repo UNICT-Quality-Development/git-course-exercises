3b4717af0a8c2b8c02c06ac1371e2eca1aefc939
Celeste2255 <giuliamaugeri.gm@gmail.com>
2022-10-19 18:37:08 +0200

    feat: switch_case implemented

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -12,38 +12,47 @@ int main()
   cout << "Scrivere giorno della settimana (lunedì=1, domenica=7):" << endl;
   cin >> day;
 
-  if (day == 1)
-  {
+switch(day){ 
+  case <day == 1> :
+  
     cout << "Lunedì" << endl;
-  }
-  else if (day == 2)
-  {
-    cout << "Martedì" << endl;
-  }
-  else if (day == 3)
-  {
-    cout << "Mercoledì" << endl;
-  }
-  else if (day == 4)
-  {
-    cout << "Giovedì" << endl;
-  }
-  else if (day == 5)
-  {
-    cout << "Venerdì" << endl;
-  }
-  else if (day == 6)
-  {
-    cout << "Sabato" << endl;
-  }
-  else if (day == 7)
-  {
-    cout << "Domenica" << endl;
-  }
-  else
-  {
-    cout << "Qualcosa è andato storto. Per favore, inserisci un numero compreso tra 1-7." << endl;
-  }
+    break;
 
+  case <day == 2>:
+  
+    cout << "Martedì" << endl;
+    break;
+
+  case <day == 3>:
+  
+    cout << "Mercoledì" << endl;
+    break;
+
+  case <day == 4>:
+  
+    cout << "Giovedì" << endl;
+    break;
+  
+  case <day == 5>:
+  
+    cout << "Venerdì" << endl;
+    break;
+  
+  case <day == 6>:
+  
+    cout << "Sabato" << endl;
+    break;
+  
+  case <day == 7>:
+  
+    cout << "Domenica" << endl;
+    break;
+
+  default:
+
+    cout << "Qualcosa è andato storto. Per favore, inserisci un numero compreso tra 1-7." << endl;
+  
+}
+ 
   return EXIT_SUCCESS;
 }
