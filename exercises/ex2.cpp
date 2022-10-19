cc6955b11f7881e5c344974a11e8b27b3fd4028c
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-10-19 18:48:50 +0200

    Substitution If/else with array

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -1,51 +1,18 @@
-/*
- Improve this program replacing if/else if with an array.
-
- Hint:  arr[3] = "Thursday";
-*/
-
+/* Hint:  arr[3] = "Thursday"*/
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int week;
+int main(){
+  unsigned int week;
 
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
+  string arr[] = { "Monday","Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"};
+  if (week < 8 && week > 0) {
+	  cout << arr[week - 1] << endl;
   }
   else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
-
+	cout << "Invalid input! Please enter week number between 1-7." << endl;
   return 0;
 }
