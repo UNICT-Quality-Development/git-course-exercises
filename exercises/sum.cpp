59710ae8305d70f5e41413f384718a52cada0122
samu <j3mtek@gmail.com>
2022-10-19 18:32:52 +0200

    sum code implementation

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,33 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+//functions prototype
+
+float sum(float a, float b);
+
+//Main
+int main(void)
+{
+  float first_in, second_in, result;
+
+  cout << "Insert the first value: ";
+  cin >> first_in;
+  cout << "Insert the second value: ";
+  cin >> second_in;
+  result = sum(first_in, second_in);
+  cout<< "the result is " << result;
+  cout<< "\n";
+
+  return 0;
+}
+
+//functions code
+
+float sum(float a, float b)
+{
+  float sum = a+b;
+  return sum;
+}
