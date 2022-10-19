b77cd9a3bc9a08a8f09e151a54765ae805218b4b
Frica Badha <fexbadu@gmail.com>
2022-10-19 18:17:15 +0200

    feat: add binary converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,39 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+
+/*
+8, lo devo dividere per 2
+- per ogni suddivisione conservare il resto (se è zero o uno)
+- 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int resto[32];
+
+int main() {
+
+  int a;
+  cout << "Insert a number: ";
+  cin >> a;
+
+  
+  int i = 0;
+  while(a > 0) {
+    resto[i] = a%2;
+    i++;
+    a /= 2;
+  }
+
+  for(int j = 31; j >= 0; j--){
+    cout << resto[j];
+  }
+  
+  cout << "\n";
+}
+
+
