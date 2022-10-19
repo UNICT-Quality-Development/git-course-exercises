113d12ad185352d8b062bc14ca07cbb8c8a477dd
Bianca Giorgio <biancagiorgio987@gmail.com>
2022-10-19 18:36:08 +0200

    File somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,8 +1,25 @@
-/*
-  Write a program that takes as input two numbers and print the sum.
-
-  Output:
-  Insert the first number: 1
-  Insert the second number: 2
-  Sum: 3
-*/
+#include <iostream>
+using namespace std;
+int sum(int a,int b){return a+b;}
+float sum(float a,float b){return a+b;}
+int main(){
+	int a,b;
+	float a2,b2;
+	string operazione;
+	cout<<"Vuoi inserire un numero intero(scrivi 'int')"<<endl<<"Vuoi inserire un numero con la varigola(scrivi 'float')"<<endl;
+	cin>>operazione;
+	if(operazione=="int"){
+		cout<<"Inserire il primo numero: ";
+		cin>>a;
+		cout<<"Inserire il secondo numero: ";
+		cin>>b;
+		cout<<"La somma di "<<a<<"+"<<b<<"="<<sum(a,b);
+	}
+	else if(operazione=="float"){
+		cout<<"Inserire il primo numero: ";
+		cin>>a2;
+		cout<<"Inserire il secondo numero: ";
+		cin>>b2;
+		cout<<"La somma di "<<a2<<"+"<<b2<<"="<<sum(a2,b2);
+	}
+}
