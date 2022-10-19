37ab2525de0648093e85cab0602ccb75135e8803
Luca Strano <stranoluca469955@gmail.com>
2022-10-19 18:30:47 +0200

    chore: generate random number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,16 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+using namespace std;
+
+#define EXIT_SUCCESS 0
+
+int main(){
+
+  double x = rand();
+
+  cout << "The random number is: " << x << endl;
+  return EXIT_SUCCESS;
+}
