f52e180d078455ce3baaf6cd234ecd50cd89390e
DefEnge <59794331+DefEnge@users.noreply.github.com>
2022-10-19 18:51:20 +0200

    feat: Implemented ex2

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -5,47 +5,23 @@
 */
 
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main()
 {
   int week;
 
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+  const char* a[7]= {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
-
+  cout<<"Enter a week number (1-7)"<<endl;
+  cin>>week; 
+  if(week <1 || week>7){  
+    cout<<"error "<<endl;
+    return 0; 
+  } 
+  
+  cout<<a[week-1]<<endl;;
+  
   return 0;
 }
