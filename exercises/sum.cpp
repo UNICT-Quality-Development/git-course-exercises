8c83143672330caad167743d944a4b19f0b1d902
Andrei Stefan <andry4you@hotmail.it>
2022-10-19 17:32:28 +0200

    feat: implemented sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,26 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T sum (T n1, T n2){
+  return n1+n2;
+}
+
+int main(){
+
+  double n1,n2;
+  cout << "Inserire il primo numero:";
+  cin >> n1;
+
+  cout << "Inserire il secondo numero:";
+  cin >> n2;
+
+  cout << "La somma fra i due numeri e': " << sum<double>(n1,n2);
+
+  return 0;
+}
