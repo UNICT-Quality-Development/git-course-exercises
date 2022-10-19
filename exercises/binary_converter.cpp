52c8ae54e323db28142cdd3317279aaa248a9d38
Peppe Aiello <giuseppe8005@gmail.com>
2022-10-19 18:54:46 +0200

    feat: esercizio binaryconverter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,27 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a[10], n, i;
+
+  cout << "Inserisci il numero da convertire: ";
+  cin >> n;
+
+  for(i=0; n>0 ; i++){
+
+    a[i] = n%2;
+    n=n/2;
+  }
+
+  cout << "Il numero convertito è: ";
+
+  for(i=i-1; i>=0 ;i--){
+    cout <<  a[i];
+
+  }
+
+  cout << endl;
+}
