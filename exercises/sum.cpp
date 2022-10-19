c0364d80846ebe3bd8ce8900c0aa0df9f3d31c2e
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-10-19 17:44:35 +0200

    Implementazione somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename X>
+
+T somma(T a, X b) {
+	return a + b;
+}
+
+int main() {
+	int a, b;
+	cout << "Inserire primo addendo: " << endl;
+	cin >> a;
+	cout << "Inserire secondo addendo: " << endl;
+	cin >> b;
+	cout << "La somma di " << a << " e " << b << " e' pari a: " << somma(a, b);
+	return 0;
+}
 
