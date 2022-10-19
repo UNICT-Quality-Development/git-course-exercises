3b9e90a7a59fcaee6434a7064a376732fd5beef3
Salvo <salvoscavo49@gmail.com>
2022-10-19 17:20:39 +0200

    feat(sum):Add main and sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,22 @@
   Insert the second number: 2
   Sum: 3
 */
+#include<iostream>
+using namespace std;
+
+int sumInt(int n1,int n2)
+{
+   return n1+n2;
+}
+
+int main()
+{
+  int n1,n2;
+  cout<<"Insert first number: ";
+  cin>>n1;
+  cout<<"Insert the second number: ";
+  cin>>n2;
+
+  int sum=sumInt(n1,n2);
+  cout<<"Sum: "<<sum<<endl;
+}
