60658000e73d38318673b9763db9d1c516b2f581
zG4bry <gabrielebalsamo2003@gmail.com>
2022-10-19 18:14:25 +0200

    somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,20 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
