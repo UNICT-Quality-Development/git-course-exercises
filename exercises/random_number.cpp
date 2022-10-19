71ad1474f3d4cbbfb8aa5db7f0d8f1c37b4c459c
weelics <weelixhill@gmail.com>
2022-10-19 18:55:08 +0200

    feat: add random function

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,25 @@
   Output:
   The random number is: 4
 */
+/*
+  Write a program that generates a random number.
+
+  Output:
+  The random number is: 4
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+
+int getRandom(){
+  srand(time(NULL));
+  return rand();
+}
+
+int main(){
+  cout<< getRandom()<<endl;
+  return 0;
+}
