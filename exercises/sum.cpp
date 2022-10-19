0505c066b9b2b59e47d955711aacef7c624e649d
Frica Badha <fexbadu@gmail.com>
2022-10-19 17:40:09 +0200

    feat: add sum exercises

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  cout << "Enter first number: ";
+  cin >> a;
+  cout << "and second number: ";
+  cin >> b;
+  int sum = a+b;
+  cout << "The sum is: " <<  sum << "\n";
+}
