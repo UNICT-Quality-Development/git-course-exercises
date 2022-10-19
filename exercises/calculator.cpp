1c08cb82885648762b46201224d33d23b6b30ca1
Giancarmelo <giancarmelo99@gmail.com>
2022-10-19 18:08:29 +0200

    esercizio calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,24 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  double num1 = 0, num2 = 0;
+
+  cout << "Insert first number: ";
+  cin >> num1;
+  cout << "\nInsert second number: ";
+  cin >> num2;
+  cout << endl;
+
+  cout << "SUM: " << num1 + num2;
+  cout << "\nDifference: " << num1 - num2;
+  cout << "\nMultiplication: " << num1 * num2 ;
+  cout << "\nDivision: " << num1 / num2;
+
+  return 0;
+}
+
