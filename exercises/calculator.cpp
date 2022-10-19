2058305c852e37687fffa79732b10aceea8854e6
Luca Strano <stranoluca469955@gmail.com>
2022-10-19 17:52:15 +0200

    feat: create new functions

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,39 @@
   Multiplication: 8
   Division: 2
 */
+
+double sum(double a, double b){
+  return a+b;
+}
+
+double difference(double a, double b){
+  return a-b;
+}
+
+double moltiplication(double a, double b){
+  return a*b;
+}
+
+double division(double a, double b){
+  return a/b;
+}
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  double x, y;
+
+  cout<<"Insert first number: ";
+  cin>>x;
+
+  cout<<"Insert second number: ";
+  cin>>y;
+
+  cout<<"SUM: "<<sum(x, y)<<endl;
+  cout<<"Difference: "<<difference(x, y)<<endl;
+  cout<<"Moltiplication: "<<moltiplication(x, y)<<endl;
+  cout<<"Division: "<<division(x, y)<<endl;
+
+  return 0;
+}
