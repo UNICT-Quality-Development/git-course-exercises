d38b133728d40a0f524a26ec1477c1cf89b3c3b0
DefEnge <59794331+DefEnge@users.noreply.github.com>
2022-10-19 17:31:05 +0200

    feat:Implemented Calculator script

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,24 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int x,y=0; 
+
+  cout<<"insert two numbers"<<endl;  
+  //take input
+  cin>>x; 
+  cin>>y; 
+  //print output
+  cout<<"sum "<<x+y<<endl;
+  cout<<"difference "<<x-y<<endl;
+  cout<<"Multiplication "<<x*y<<endl;
+  cout<<"Division "<<x/y<<endl;
+
+  return 0;
+}
