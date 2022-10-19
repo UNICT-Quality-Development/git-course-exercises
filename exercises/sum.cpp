c9b43bc1af338019fa20c7e94cb56ecf105ce028
Real96 <chicco.gaeta@gmail.com>
2022-10-19 17:55:53 +0200

    feat: added sum function

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,8 +1,21 @@
-/*
-  Write a program that takes as input two numbers and print the sum.
+#include <iostream>
 
-  Output:
-  Insert the first number: 1
-  Insert the second number: 2
-  Sum: 3
-*/
+using namespace std;
+
+double sum(double x, double y) {
+  return x + y;
+}
+
+int main() {
+  double x, y;
+
+  cout << "Insert the first number: ";
+  cin >> x;
+
+  cout << "Insert the second number: ";
+  cin >> y;
+
+  cout << "Sum: " << sum(x, y) << endl;
+
+  return 0;
+}
