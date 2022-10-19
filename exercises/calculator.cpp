dd61f550d25234a0c05530df6494a473bafc216a
onitnas <santinoisgr7@gmail.com>
2022-10-19 18:17:12 +0200

    feat: svolgimento calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,40 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+#define EXIT_SUCCESS 0
+
+using namespace std;
+
+int sum(int n1, int n2)
+{
+  return n1 + n2;
+}
+int difference(int n1, int n2)
+{
+  return n1 - n2;
+}
+int multiplication(int n1, int n2)
+{
+  return n1 * n2;
+}
+int division(int n1, int n2)
+{
+  return n1 / n2;
+}
+
+int main()
+{
+  int n1, n2;
+  cout << "Inserisci il primo numero: ";
+  cin >> n1;
+  cout << "Inserisci il secondo numero: ";
+  cin >> n2;
+  cout << "La somma tra i due numeri e': " << sum(n1, n2) << endl;
+  cout << "La differenza tra i due numeri e': " << difference(n1, n2) << endl;
+  cout << "La moltiplicazione tra i due numeri e': " << multiplication(n1, n2) << endl;
+  cout << "La divisione intera tra i due numeri e': " << division(n1, n2) << endl;
+  
+  return EXIT_SUCCESS;
+}
