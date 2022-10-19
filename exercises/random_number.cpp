492fd38e098a389f0a45de1b68e1b104f9c4c4b1
Andrei Stefan <andry4you@hotmail.it>
2022-10-19 17:40:08 +0200

    feat:implemented random number generator

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,15 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+  srand(time(0));
+
+  cout << "The random number is: " << rand() << endl;
+}
