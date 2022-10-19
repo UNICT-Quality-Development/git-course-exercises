8183a93a6a9d4247d4f494e20970fcff283439cf
Celeste2255 <giuliamaugeri.gm@gmail.com>
2022-10-19 17:47:07 +0200

    feat: somma indecisa

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+void main() {
+  int a, b, sum;
+  
+  cout<<'Inserire un numero';
+  cin>>a;
+  cout<<'Inserire secondo numero';
+  cin>>b;
+
+  sum=0;
+  sum=a+b;
+  cout<<'La somma dei due numeri è: <<somma';
+
+}
