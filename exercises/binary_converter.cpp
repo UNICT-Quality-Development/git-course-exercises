66edc83d60ecc8c67b979eff4920279ce786e817
Emanuel Bisicchia <emanuel.bisicchia99@gmail.com>
2022-10-19 18:35:19 +0200

    feat: a binary converter in cpp

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,29 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10], n, i;
+
+    // prendo il numero da convertire in binario
+    cout << "Insert first number: ";
+    cin >> n;
+
+    // mi ricavo il binario del numero
+    for (i = 0; n > 0; i++) {
+        a[i] = n % 2;
+        n = n / 2;
+    }
+
+    // stampo il numero
+    cout << "The binary number is: ";
+    for (i = i - 1; i >= 0; i--) {
+        cout << a[i];
+    }
+
+    cout << endl;
+    return 0;
+}
