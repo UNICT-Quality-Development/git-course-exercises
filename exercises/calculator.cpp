4f86c4b7d682e0be5b8b4dd7cf26a8a9f0e87426
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-10-19 18:43:15 +0200

    feat: implemented calculator.cpp

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -5,8 +5,30 @@
   Insert first number: 4
   Insert second number: 2
 
-  SUM: 6
+  Sum: 6
   Difference: 2
   Multiplication: 8
   Division: 2
 */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+float x, y;
+
+int main(int argc, char **argv){
+	printf("First number: ");
+	scanf("%f", &x);
+
+	printf("Second number: ");
+	scanf("%f", &y);
+
+	printf("\n");
+	printf("Sum: %.2f\n", x + y);
+	printf("Difference: %.2f\n", x - y);
+	printf("Multiplication: %.2f\n", x * y);
+	printf("Division: %.2f\n", x / y);
+
+	return 0;
+}
