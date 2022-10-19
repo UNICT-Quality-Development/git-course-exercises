ea38ca851f735e3503f5e3a769c78931b94cf0a5
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-10-19 17:44:25 +0200

    feat: added sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,20 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int first_number, second_number, sum;
+
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  sum = first_number + second_number;
+
+  cout << first_number << " + " << second_number << " = " << sum;
+
+  return 0;
+}
