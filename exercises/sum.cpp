3be79ffa8aa0d24161203034841313ae52278778
Carmelo Fuccio <carmelofuccio22@gmail.com>
2022-10-19 17:46:50 +0200

    file:sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,30 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+
+int sum(int a,int b){
+
+	return (a+b);
+
+}
+
+int main(){
+
+	int a,b;
+
+	cout << "Insert the first number" << endl;	
+	cin >> a;
+	cout << "Insert the second number" << endl;
+	cin >> b;
+
+	cout << "Sum of " << a << " and " << b << endl;
+	int resSum=sum(a,b);
+
+	return 0;
+
+}
