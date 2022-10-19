501aa7cdcac6ab02b5351dd48850a7c52ab31efe
Giancarmelo <giancarmelo99@gmail.com>
2022-10-19 18:40:27 +0200

    feat: implement random instruction

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,16 @@
   Output:
   The random number is: 4
 */
+#include <cstdlib>
+#include <iostream>
+
+#define EXIT_SUCCESS 0
+
+using namespace std;
+
+int main() {
+
+  cout << "The random number id: " << rand() % 300;
+
+  return EXIT_SUCCESS;
+}
