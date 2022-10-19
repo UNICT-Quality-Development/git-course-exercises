01685e1d2632b53b21adf6905bf0ad214db7143d
Frica Badha <fexbadu@gmail.com>
2022-10-19 18:36:55 +0200

    fix: refactor

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -8,16 +8,18 @@
 */
 
 #include <iostream>
+#define EXIT_SUCCESS 0
 
 using namespace std;
 
-int main(){
-  int a;
-  int b;
+int main() {
+  int a, b;
   cout << "Enter first number: ";
   cin >> a;
   cout << "and second number: ";
   cin >> b;
-  int sum = a+b;
+  int sum = a + b;
   cout << "The sum is: " <<  sum << "\n";
+
+  return EXIT_SUCCESS;
 }
