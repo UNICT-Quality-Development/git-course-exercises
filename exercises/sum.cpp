aba6ec774b2cdc68c4702b1fe11afd2d8ac35ddf
Emanuel Bisicchia <emanuelbisicchia@MacBook-Air-di-Emanuel.local>
2022-10-19 17:54:03 +0200

    feat: sum between two integers

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,24 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // prendo in input i due numeri
+    cout << "Insert the first number: ";
+    cin >> num1;
+    cout << "Insert the second number: ";
+    cin >> num2;
+
+    // sommo i due numeri
+    sum = num1 + num2;
+
+    // stampo la somma
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
