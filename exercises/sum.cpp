317afb9bb1ad12316842b2b90af139cc64b8e817
Luca Strano <stranoluca469955@gmail.com>
2022-10-19 17:33:02 +0200

    feat: implemented new function sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,23 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+double sum(double a, double b){
+  return a+b;
+}
+
+int main(){
+  double x, y;
+
+  cout<<"Insert first number: ";
+  cin>>x;
+
+  cout<<"Insert second number: ";
+  cin>>y;
+
+  cout<<"La somma dei due numeri e': "<<sum(x, y)<<endl;
+  return 0;
+}
