49655026ca41d78f648872af38ec13a0ea66340f
Tommaso Dirosa <DRSTMS00L12B428D@studium.unict.it>
2022-10-19 17:53:33 +0200

    prova

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,27 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include<iostream>
+
+float sum(float num1, float num2){
+
+  return num1 + num2;
+
+}
+
+int main(){
+ float  num1, num2;
+ std::cout << "Insert the first number: ";
+ std::cin >> num1 ;
+
+ std::cout << "Insert the first number: ";
+ std::cin >> num2 ;
+ float ris = sum(num1, num2);
+ std::cout << "Sum: " <<ris  <<std::endl; 
+  
+
+}
+
+
+
