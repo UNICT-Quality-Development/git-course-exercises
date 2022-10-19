615a0fc4b64349c2bd47cc43f0c72e8cefa67eeb
Giancarmelo <giancarmelo99@gmail.com>
2022-10-19 17:17:20 +0200

    implementazione funzione sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,26 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+
+using namespace std;
+
+
+int sum(int n, int n1){
+  return n + n1;
+}
+
+void main(){
+  int n, n1;
+
+  cout << "Insert the first number:";
+  cin >> n;
+  cout << endl;
+
+  cout << "Insert the second number:";
+  cin >> n1;
+  cout << endl;
+
+  cout << "Sum:" << sum(n,n1);
+
+}
