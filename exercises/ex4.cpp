b73be28a8f8a999f43c3bfdab408474d00cf7635
Giancarmelo <giancarmelo99@gmail.com>
2022-10-19 18:48:14 +0200

    refactor: main

--- exercises/ex4.cpp
+++ exercises/ex4.cpp
@@ -10,47 +10,18 @@ int main()
   printf("Enter month number(1-12): ");
   scanf("%d", &month);
 
-  switch (month)
-  {
-  case 1:
-    printf("31 days");
-    break;
-  case 2:
+  if ( month == 2 ) 
     printf("28/29 days");
-    break;
-  case 3:
+
+  else if ( month % 2 == 1)
     printf("31 days");
-    break;
-  case 4:
+
+  else if ( month % 2 == 0)
     printf("30 days");
-    break;
-  case 5:
-    printf("31 days");
-    break;
-  case 6:
-    printf("30 days");
-    break;
-  case 7:
-    printf("31 days");
-    break;
-  case 8:
-    printf("31 days");
-    break;
-  case 9:
-    printf("30 days");
-    break;
-  case 10:
-    printf("31 days");
-    break;
-  case 11:
-    printf("30 days");
-    break;
-  case 12:
-    printf("31 days");
-    break;
-  default:
+
+  else
     printf("Invalid input! Please enter month number between 1-12");
-  }
+
 
   return 0;
 }
