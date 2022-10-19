9ac8374016b630e0def2fa5d793346495c482523
Salvo <salvoscavo49@gmail.com>
2022-10-19 17:37:46 +0200

    Feat: improve code

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -1,48 +1,45 @@
 /* Improve this program using a switch-case. */
 
 #include <iostream>
+#include <string.h>
 using namespace std;
+
+string getDay(int w)
+{
+  switch (w)
+  {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Thuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6: 
+      return "Saturday";
+    case 7: 
+      return "Sunday";
+    default:
+      cout << "Invalid input! Please enter week number between 1-7." << endl;
+      return "Err";
+      break;
+  }
+}
 
 int main()
 {
   int week;
-
+  string day;
   /* Input week number from user */
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+  do
+  {
+    cout << "Enter week number(1-7): " << endl;
+    cin >> week;
+  } while (week<1 || week > 7);
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
-
+  cout<<getDay(week);
   return 0;
 }
