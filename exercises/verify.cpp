c91d6ce608a4dbd23740bec5d32de65af382f7da
Salvo <salvoscavo49@gmail.com>
2022-10-20 13:08:42 +0200

    feat: add checkNumber function

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -9,10 +9,34 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
+#define EXIT_SUCCESS 0
+#define DIM 10
+
+bool checkNumber(int* arr,int num)
+{
+  for(int i=0;i<DIM;i++)
+  {
+    if(i == num )
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main()
 {
   // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int N[DIM] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
-  return 0;
+  int num;
+  cout<<"Insert number: ";
+  cin>>num;
+
+  if(checkNumber(N,num))
+    cout<<"The number "<< num <<" is present in the array \n";
+  else 
+    cout<<"The number "<< num <<" is not present in the array \n";
+
+  return EXIT_SUCCESS;
 }
