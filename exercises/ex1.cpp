32f81d9d99cc47b3c877a62cd64c11f0451a0622
DefEnge <59794331+DefEnge@users.noreply.github.com>
2022-10-25 10:43:35 +0200

    fixed exercise 1

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 using namespace std;
+#define EXIT_SUCCESS 0 
 
 int main()
 {
@@ -13,53 +14,38 @@ int main()
   switch (week)
   {
   case 1:
-
     cout << "Monday" << endl;
-  
   break;
   
   case 2: 
-
     cout << "Tuesday" << endl;
-
   break;
 
   case 3: 
-  
     cout << "Wednesday" << endl;
-  
   break;
 
 
   case 4: 
-
     cout << "Thursday" << endl;
-
   break;
 
   case 5: 
-
     cout << "Friday" << endl;
-
   break;
 
   case 6: 
-  
     cout << "Saturday" << endl;
-
   break;
 
   case 7: 
-
     cout << "Sunday" << endl;
-  
   break;
 
   default:
-  
     cout << "Invalid input! Please enter week number between 1-7." << endl;  
     break;
   }
   
-  return 0;
+  return EXIT_SUCCESS;
 }
