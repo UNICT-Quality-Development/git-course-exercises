9ba63c27d3468bfb706c483f88382954558a18a3
Christian <san.chri@tiscali.it>
2022-11-10 13:07:09 +0100

    refactor: replacing if-else with an array

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -5,47 +5,25 @@
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-  int week;
+  	int week;
+	string arr[7]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+ 	cout << "Enter week number(1-7): " << endl;
+  	cin >> week;
 
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+	if (week>0&&week<8)
+	{
+		cout << arr[week-1] << endl;
+	}
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+  	else
+  	{
+    		cout << "Invalid input! Please enter week number between 1-7." << endl;
+  	}
 
   return 0;
 }
