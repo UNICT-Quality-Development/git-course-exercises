448ae0fea1ebf427b08d70025b2ef5d76c6d9eed
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-11 11:47:07 +0100

    implementation of binary converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -1,7 +1,22 @@
-/*
-  Write a program that given a number as input convert it in binary.
+#include <iostream>
+using namespace std;
 
-  Output:
-  Insert first number: 8
-  The binary number is: 1000
-*/
+double convert(int n) {
+	double bin = 0;
+	int rem, i = 1;
+
+	while (n != 0) {
+		rem = n % 2;
+		n /= 2;
+		bin += rem * i;
+		i *= 10;
+	}
+	return bin;
+}
+
+int main() {
+	int x;
+	cout << "Enter a number: " << endl;
+	cin >> x;
+	cout << "The binary equivalent of " << x << " is: " << convert(x) << endl;
+}
