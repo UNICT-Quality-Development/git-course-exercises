33954b32370a70c677d49b81575ff316a3c99bd3
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-11 12:19:34 +0100

    test do while

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -1,6 +1,4 @@
-/*
-  Write a program that given two numbers as input make the main operations.
-
+/* Write a program that given two numbers as input make the main operations.
   Output:
   Insert first number: 4
   Insert second number: 2
@@ -8,5 +6,41 @@
   SUM: 6
   Difference: 2
   Multiplication: 8
-  Division: 2
-*/
+  Division: 2	*/
+
+#include <iostream>
+using namespace std;
+
+double sum(double x, double y) {
+	return x + y;
+}
+
+double diff(double x, double y) {
+	return x - y;
+}
+
+double mul(double x, double y) {
+	return x * y;
+}
+
+double div(double x, double y) {
+	return x / y;
+}
+
+int main() {
+	double x, y = 0;
+	char op;
+	cout << "Insert first number: " << endl;
+	cin >> x;
+	cout << "Insert second number: " << endl;
+	cin >> y;
+	do {
+		cout << "Main operation: +, -, *, /" << endl;
+		cin >> op;
+	} while (op != '+' || op != '-' || op != '*' || op != '/') {
+		cout << "Invalid operator" << endl;
+		cin >> op;
+	}
+
+	return EXIT_SUCCESS;
+}
