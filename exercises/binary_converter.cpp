a7d5b83e0154ea1fdbc6a121366725915f6d4c5d
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-12 12:35:36 +0100

    binary_converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #define EXIT_SUCCESS 0
 using namespace std;
-
 double convert(int n) {
-	double bin = 0;
-	int rem, i = 1;
-
-	while (n != 0) {
-		rem = n % 2;
-		n /= 2;
-		bin += rem * i;
-		i *= 10;
-	}
-	return bin;
+double bin = 0;
+int rem, i = 1;
+while (n != 0) {
+rem = n % 2;
+n /= 2;
+bin += rem * i;
+i *= 10;
 }
-
+return bin;
+}
 int main() {
-	int x;
-	cout << "Enter a number: " << endl;
-	cin >> x;
-	cout << "The binary equivalent of " << x << " is: " << convert(x) << endl;
-	return EXIT_SUCCESS;
+int x;
+cout << "Enter a number: " << endl;
+cin >> x;
+cout << "The binary equivalent of " << x << " is: " << convert(x) << endl;
+return EXIT_SUCCESS;
 }
-	
