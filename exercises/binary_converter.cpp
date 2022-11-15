ed802a4b68910cadeb1f401e0f0caac35faec93f
Antonello R. D <111999315+MivalRavenhoot@users.noreply.github.com>
2022-11-15 14:41:41 +0100

    aggiunta indentazione

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #define EXIT_SUCCESS 0
 using namespace std;
+
 double convert(int n) {
-double bin = 0;
-int rem, i = 1;
-while (n != 0) {
-rem = n % 2;
-n /= 2;
-bin += rem * i;
-i *= 10;
+    double bin = 0;
+    int rem, i = 1;
+    while (n != 0) {
+        rem = n % 2;
+        n /= 2;
+        bin += rem * i;
+        i *= 10;
+    }
+    return bin;
 }
-return bin;
-}
+
 int main() {
-int x;
-cout << "Enter a number: " << endl;
-cin >> x;
-cout << "The binary equivalent of " << x << " is: " << convert(x) << endl;
+    int x;
+    cout << "Enter a number: " << endl;
+    cin >> x;
+    cout << "The binary equivalent of " << x << " is: " << convert(x) << endl;
 return EXIT_SUCCESS;
 }
