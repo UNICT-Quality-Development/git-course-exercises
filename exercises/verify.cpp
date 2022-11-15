6279760d2077ac9f4cca3a106da309e695dec3ac
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-15 11:28:30 +0100

    Fix: removed unnecessary space

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -8,19 +8,19 @@ The number 3 is [not] present in the array.
 using namespace std;
 
 int main() {
-	// placeholder
-	int N[10] = { 3, 4, 5, 1, 2, 3, 4, 9, 13, 0 };
-	int a = 0;
-	bool check = false;
-	cout << "insert number: ";
-	cin >> a;
-	for (int i = 0; i < 5; i++) {
-		if (a == N[i] || a == N[i + 5])
-			check = true;
-	}
-	if (check)
-		cout << "The number " << a << " is present in the array" << endl;
-	else
-		cout << "The number " << a << " is NOT present in the array" << endl;
-	return EXIT_SUCCESS;
+// placeholder
+int N[10] = { 3, 4, 5, 1, 2, 3, 4, 9, 13, 0 };
+int a = 0;
+bool check = false;
+cout << "insert number: ";
+cin >> a;
+for (int i = 0; i < 5; i++) {
+if (a == N[i] || a == N[i + 5])
+check = true;
+}
+if (check)
+cout << "The number " << a << " is present in the array" << endl;
+else
+cout << "The number " << a << " is NOT present in the array" << endl;
+return EXIT_SUCCESS;
 }
