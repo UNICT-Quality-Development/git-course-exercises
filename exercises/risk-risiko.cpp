e4069b0374ec4f3d6aa66371cc07fea877b0ba23
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-15 16:43:06 +0100

    implementazione Risk-risiko"

--- exercises/risk-risiko.cpp
+++ exercises/risk-risiko.cpp
@@ -1,5 +1,4 @@
-/*
-  Write a program that simulates a risk/risiko fight using 6 dices.
+/*  Write a program that simulates a risk/risiko fight using 6 dices.
 
   How does it work?
   When a player attacks another player he uses 3 dices, the red is always the attacker and the blue is the defender.
@@ -27,3 +26,52 @@
   M 3 vs 3 => blue win
   O 2 vs 1 => red win
 */
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionsort(int array[], int n) {
+    int indexMax;
+    for (int i = 0; i < n; i++) {
+        indexMax = i;
+        for (int j = i + 1; j < n; j++) {
+            if (array[j] > array[indexMax])
+                indexMax = j;
+        }
+        swap(array[i], array[indexMax]);
+    }
+}
+
+int main() {
+    srand(time(0));
+    int Red[3] = { rand() % 7,rand() % 7 ,rand() % 7 };
+    int Blue[3] = { rand() % 7,rand() % 7 ,rand() % 7 };
+    bool flag;
+    selectionsort(Red, 3);
+    selectionsort(Blue, 3);
+    cout << "Red dices: \n" << Red[0] << " (N) \n"
+        << Red[1] << " (M) \n"
+        << Red[2] << " (O) \n\n";
+
+    cout << "Blue dices: \n" << Blue[0] << " (N) \n"
+        << Blue[1] << " (M) \n"
+        << Blue[2] << " (O) \n\n";
+
+    for (int i = 0; i < 3; i++) {
+        flag = false;
+        if (Red[i] > Blue[i])
+            flag = true;
+        cout << Red[i] << " vs " << Blue[i] << " ==> ";
+        if (flag)
+            cout << "Red Wins! \n";
+        else
+            cout << "Blue Wins! \n";
+    }
+    return EXIT_SUCCESS;
+}
