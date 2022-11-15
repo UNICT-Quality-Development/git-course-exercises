7bc36572d1e7e4e3b5e706cbcee2f641b1a879ff
Antonello Romanelli <antonelloromanelli@hotmail.it>
2022-11-15 11:26:51 +0100

    Implemented request

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -1,6 +1,4 @@
-/*
-Write a software that verifies if a number is present in a pre-defined array.
-
+/*Write a software that verifies if a number is present in a pre-defined array.
 Output example:
 Insert number 3
 The number 3 is [not] present in the array.
@@ -9,10 +7,20 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
-
-  return 0;
+int main() {
+	// placeholder
+	int N[10] = { 3, 4, 5, 1, 2, 3, 4, 9, 13, 0 };
+	int a = 0;
+	bool check = false;
+	cout << "insert number: ";
+	cin >> a;
+	for (int i = 0; i < 5; i++) {
+		if (a == N[i] || a == N[i + 5])
+			check = true;
+	}
+	if (check)
+		cout << "The number " << a << " is present in the array" << endl;
+	else
+		cout << "The number " << a << " is NOT present in the array" << endl;
+	return EXIT_SUCCESS;
 }
