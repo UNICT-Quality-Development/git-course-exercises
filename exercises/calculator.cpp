ef1b4339c2a1d51c09cd62d179fe158e7cbe0926
test <test@test.com>
2023-10-18 18:50:53 +0200

    feat: per favore non piangere

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,27 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+float calculator(float a,float b,int operation){
+  switch(operation){
+    case 0: return a+b;
+    break;
+    case 1: return a-b;
+    break;
+    case 2: return a/b;
+    break;
+    case 3: return a*b;
+    break;
+    default:
+    cout<<"invalid operator\n";
+    return 0.0;
+  }
+}
+
+int main(){
+  srand(time(0));
+  cout<<calculator((rand()%100),rand()%100,0)<<endl;
+}
--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,6 +6,13 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
 int sum(int a,int b){
 	return a+b;
+}
+
+int main(){
+	cout<<sum(5,6)<<endl;
 }
