e06cbf6ef98af7aeb5a010f949cdd3f8e0ed5ad9
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-18 18:43:11 +0200

    feat:verify done

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -11,8 +11,16 @@ using namespace std;
 
 int main()
 {
-  // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
-
-  return 0;
+	int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+	int Target, i;
+	cout << "insert number: ";
+	cin>>Target;
+	for(i=0;i<10;i++){
+		if(N[i]==Target){
+			cout << "The number " << Target <<" is present in the array." << endl;
+			return 0;
+		}
+	}
+	cout << "The number " << Target << " is not present in the array." << endl;
+	return 0;
 }
