3d3cba8a4de3c14073a763a2b59cc7f95cac81d4
Vulcalien <vulcalien@vulcalien.net>
2023-10-18 18:07:02 +0200

    feat: improve 'compile.sh'
    Previously, the compile.sh script compiled without linking (-c option
    for g++) generating non-executable object files. This commit makes the
    script compile *and link* all the exercise files.
    The executable files will also be put in the 'bin' subdirectory.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-for FILE in exercises/*.cpp; do
+
+SCRIPT_DIR=$(dirname $0)
+BIN_DIR=$SCRIPT_DIR/bin
+
+# create the 'bin' directory, if needed
+if [ ! -d "$BIN_DIR" ]; then
+  mkdir bin
+fi
+
+for FILE in $SCRIPT_DIR/exercises/*.cpp; do
+  # remove the .cpp extension
+  FILE_NO_EXT=$(basename "$FILE" .cpp)
+
   printf "Compiling: $FILE ->\n"
-  g++ $FILE -std=c++11 -c
+  g++ "$FILE" -std=c++11 -o "$BIN_DIR/$FILE_NO_EXT"
 done
