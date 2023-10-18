49cb65a6572007626ecc21a6bb963821b5671774
Henrysk <henriskdusk@gmail.com>
2023-10-18 18:22:10 +0200

    feat: implement calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,16 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+int main()
+{
+  long long int a, b=0;
+  std::cout << "Insert first number: ";
+  std::cin >> a;
+  std::cout << "Insert second number: ";
+  std::cin >> b;
+  std::cout << "SUM: " << a+b << std::endl 
+  << "Difference: " << a-b << std::endl 
+  << "Multiplication: " << a*b << std::endl 
+  << "Divsion: " << a/b << std::endl;
+}
