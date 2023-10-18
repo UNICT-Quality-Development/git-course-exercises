7d43f8cf3c0ba9dd4129e48a1f351f6e7263bb00
pippo <bonaventuragiuseppe1997@gmail.com>
2023-10-18 17:41:21 +0200

    feat:embled sum.c

---
+++ exercises/sum.c
@@ -0,0 +1,19 @@
+/*
+  Write a program that takes as input two numbers and print the sum.
+
+  Output:
+  Insert the first number: 1
+  Insert the second number: 2
+  Sum: 3
+*/
+#include <stdio.h>
+
+int main(){
+  //dec
+  int a,b;
+  printf("insert one number\n");
+  scanf("%d",a);
+  printf("insert one other number\n");
+  scanf("%d",a);
+  printf("Sum:%d",a+b);
+}
