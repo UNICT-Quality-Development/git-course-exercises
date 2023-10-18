b4cd67c65301bba34b22197f4c611774bab76537
calogero <calogerolupo2@gmail.com>
2023-10-18 18:51:30 +0200

    versione completa

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -13,16 +13,32 @@
 #include <stdio.h>
 
 int main(){
-  printf("Ciao, inserisci i numeri da sommare:\n");
-  double num1 = 0, num2 = 0;
-  if (scanf("%lf %lf", &num1, &num2) == 2){
-    printf("La somma vale: %g\n", num1 + num2);
-    printf("La sottrazione vale: %g\n", num1 - num2);
-    printf("La moltiplicazione vale: %g\n", num1 * num2);
-    printf("La divisione vale: %g\n", num1 / num2);
 
-  }else{
-    printf("Errore! Non hai inserito dei numeri\n");
-  }
-  
+    printf("Ciao!, Inserisci i numeri in oridne: \n");
+    double num1 = 0, num2 = 0;
+    int opz = 0;
+    if (scanf("%lf %lf", &num1, &num2) == 2){
+            printf("Scegli l'operazione che vuoi eseguire:\n1)Addizione\n2)Sottrazione\n3)Moltiplicazione\n4)Divisione\n");
+            scanf("%d", &opz);
+            switch (opz){
+            case 1:
+                printf("La somma vale: %g\n", num1 + num2);
+            break;
+            case 2:
+                printf("La sottrazione vale: %g\n", num1 - num2);
+            break;
+            case 3:
+                printf("La moltiplicazione vale: %g\n", num1 * num2);
+            break;
+            case 4:
+                printf("La divisione vale %g\n", num1 / num2);
+            break;
+            default:
+                printf("Errore! Opzione non valida\n");
+                break;
+            }        
+    }else{
+        printf("Errore! non hai inserito un numero\n");
+    }
+    
 }
