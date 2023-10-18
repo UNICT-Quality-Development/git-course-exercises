621e55e5b5aab32db923b471c26df1ab063c7eea
LorenzoMaci <lorenzomaci@virgilio.it>
2023-10-18 18:52:19 +0200

    feat: exercise calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,20 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+void operation(int a, int b){
+  cout << "SUM: " << a + b << endl;
+  cout << "Difference: " << a - b << endl;
+  cout << "Multiplication: " << a*b << endl;
+  cout << "Divsion: "<< a/b << endl;
+}
+
+int main(){
+  int a = 4;
+  int b = 2;
+  operation(a, b);
+}
