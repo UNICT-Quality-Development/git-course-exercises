5e8eade1d0ebfe93d4bdf44f7ec6122b6798c217
SCsimoc98 <scsimocarta@gmail.com>
2023-10-18 18:00:11 +0200

    refactor: improved code with an array

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -5,47 +5,23 @@
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
   int week;
+  string day[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
+  if (week < 1 || week > 7)
   {
     cout << "Invalid input! Please enter week number between 1-7." << endl;
   }
+
+  cout << day[week  - 1] << endl;
 
   return 0;
 }
