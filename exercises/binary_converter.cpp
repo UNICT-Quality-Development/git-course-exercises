a8ccc20e4b25a06e4a5845cadcc4425463ddff91
DarkFlame <matteoantonio.sangiorgio@gmail.com>
2023-10-18 18:54:17 +0200

    ho svolto l'esercizio conversione binaria

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,38 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main (){
+
+  int n;
+
+  cout <<"Inserisci un Numero ";
+
+  cin >>n;
+
+    while (n  > 0){
+      
+      if (n%2 == 0 ){
+      	
+    	cout<<0; 
+	  }
+	  
+	  else{
+	  
+	  	cout<<1;
+		}
+		// si legge al contrario
+	n = n/2;
+	}
+
+    
+}
+
+
+
