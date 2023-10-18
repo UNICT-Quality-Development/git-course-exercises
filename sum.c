f59ef0787c3dd0fd641db27ba8e8018d4eeb0936
GG <130756147+Araziocatigna@users.noreply.github.com>
2023-10-18 17:57:39 +0200

    completamento sum.c

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a, b, s;
+printf("Inserire primo numero\n");
+scanf("%d", &a);
+printf("Inserire secondo numero\n");
+scanf("%d", &b);
+s=a+b;
+printf("La somma dei due numeri e': %d", s);
+}
