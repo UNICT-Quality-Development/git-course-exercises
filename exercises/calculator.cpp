8439c6cbdf8c7304030b1042234bee2739074dec
MaccxG <MaccxG>
2023-10-18 19:03:37 +0200

    feat: addee calculator.cpp

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,22 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+  cout << "Insert first number: ";
+  cin >> x;
+  cout << "Insert second number: ";
+  cin >> y;
+
+  cout << "Sum: " << x + y;
+  cout << "Difference: " << x - y;
+  cout << "Multiplication: " << x * y;
+  cout << "Division: " << x / y;
+
+  return 0;
+}
