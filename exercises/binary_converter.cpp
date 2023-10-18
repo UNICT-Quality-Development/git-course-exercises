2095e661997f6809a5c16c47a2101dc45c1c65c5
Henrysk <henriskdusk@gmail.com>
2023-10-18 18:08:57 +0200

    feat: implements the binary converter with recursion

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,26 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+void printNumberBinary(unsigned long long int n)
+{ 
+  //Checks if we are in the base case
+  if(n<=1)
+  {
+    //if it is, just print the number (Base 2 allows only for 0s and 1s)
+    std::cout << n;
+    return;
+  }
+  //Recursion happens here
+  printNumberBinary(n/2);
+  //Print the last bit
+  std::cout << n%2;
+}
+int main()
+{
+  std::cout << "Input the number to convert to base 2" << std::endl;
+  unsigned long long m=0;
+  std::cin >> m;
+  printNumberBinary(m);
+  std::cout << std::endl;
+}
