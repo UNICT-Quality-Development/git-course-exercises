188efd59717283aab2e1c08bd96c6bb41fa69753
MatteoP <mlfplumari@gmail.com>
2023-10-18 18:22:35 +0200

    feat: simple calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,27 @@
   Multiplication: 8
   Division: 2
 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+float a, b, sum, dif, mult, div;
+
+printf("insert first number: ");
+scanf("%f", &a);
+printf("insert second number: ");
+scanf("%f", &b);
+
+sum=a+b;
+dif=a-b;
+mult=a*b;
+div=a/b;
+
+printf("SUM: %.2f\nDifference: %.2f\nMultiplication: %.2f\nDivision: %.2f\n", sum, dif, mult, div);
+
+
+exit(EXIT_SUCCESS);
+}
