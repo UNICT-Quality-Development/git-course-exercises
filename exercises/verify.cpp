7b79f3c8cc4726c470925a8cd14a850a3cf3955c
LorenzoMaci <lorenzomaci@virgilio.it>
2023-10-18 18:27:51 +0200

    feat: exercise verify

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -6,13 +6,29 @@ Insert number 3
 The number 3 is [not] present in the array.
 */
 
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
   // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int number;
+  cin >> number;
 
+  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  bool found = false;
+
+  for(int i=0; i<10; i++){
+    if(N[i] == number){
+      found = true;
+    }
+  }
+
+  if(found){
+    cout << "Insert number " << number << endl;
+  }else{
+    cout << "The number "<< number << " is [not] present in the array" << endl;
+  }
   return 0;
 }
