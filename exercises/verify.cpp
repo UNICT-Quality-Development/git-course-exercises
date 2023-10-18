a2c03d53c905603100af6cc0a0fb9dbe747f9e9b
FilippoMarletta <filippo.marletta@ctmobi.it>
2023-10-18 18:14:31 +0200

    feat(verify.cpp): implemented verify function
    implemented verify function to verify if a number is present or not into a given array

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -9,10 +9,30 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
+bool verify(int *N, int n, int val) {
+  for( int i = 0; i < n; i++) {
+    if ( N[i] == val ) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
-
+  int val;
+  cout << "Insert number : " ;
+  cin >> val;
+  
+  if( verify(N, 10, val) ) {
+    cout << "The number " << val << " is present in the array" << endl;
+  }
+  else
+  {
+    cout << "The number " << val << " is not present in the array" << endl;
+  }
+  
   return 0;
 }
