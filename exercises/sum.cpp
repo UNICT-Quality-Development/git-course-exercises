e8e12d59258e3dde878ff5a8c1a8bc5da66f65b2
Nunzio Depetro <nuccio99@hotmail.it>
2023-10-18 17:46:22 +0200

    feat: implementazione Sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b){
+return a+b;
+}
+
+int main()
+{
+int a,b;
+cout<<"inserisci il primo numero:  ";
+cin>>a;
+cout<<"inserisci il secondo numero:  ";
+cin>>b;
+cout<<"La somma dei tuoi numeri è:"<<sum(a,b);
+}
