8193d46713054e498420445a1b725d488d87f5a8
calogero <calogerolupo2@gmail.com>
2023-10-18 17:58:43 +0200

    Ho implementato sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -9,5 +9,12 @@
 #include <stdio.h>
 
 int main(){
-  printf("Ciao come stai?");
+  printf("Ciao, inserisci i numeri da sommare:\n");
+  double num1 = 0, num2 = 0;
+  if (scanf("%lf %lf", &num1, &num2) == 2){
+    printf("La somma dei numeri vale: %g\n", num1 + num2);
+  }else{
+    printf("Errore! non hai inserito dei numeri\n");
+  }
+
 }
