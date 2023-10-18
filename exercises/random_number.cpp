825acaebc904e66ab4eb6b1a9d08bd2a3699097f
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 18:10:36 +0200

    feat:implented random_number exercise

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,15 @@
   Output:
   The random number is: 4
 */
+
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+int main(){
+  srand(time(NULL));
+  cout<<"The random number is: "<<rand()<<endl;
+  return 0;
+}
