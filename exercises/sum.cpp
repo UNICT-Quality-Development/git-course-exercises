7b47c06b60e390ad670ca309398b049c9745f113
salvatoreeee <salvoneri2010@hotmail.it>
2023-10-18 17:58:28 +0200

    primo commit

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,8 +1,21 @@
-/*
-  Write a program that takes as input two numbers and print the sum.
+#include <stdio.h>
+ 
+int main()         
+{
+         int somma;
+         int num1;
+         int num2;
+ 
+         printf("Inserisci il primo valore: ");
+         scanf("%d", &num1);
+ 
+         printf("Inserisci il secondo valore: ");
+         scanf("%d", &num2);
+ 
+         somma = num1 + num2;
+ 
+         printf("La somma dei due numeri e' la seguente: %dn", somma);
+return 0;
+}
 
-  Output:
-  Insert the first number: 1
-  Insert the second number: 2
-  Sum: 3
-*/
+ 
