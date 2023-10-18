0217201963032a10357348e0e71087d348b34634
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-18 18:23:07 +0200

    feat: calcutor exercise

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,39 @@
   Multiplication: 8
   Division: 2
 */
+
+#define EXIT_SUCCESS 0
+#include <iostream>
+
+using namespace std;
+
+void calculatur(int a, int b)
+{
+    int sum, difference, multiplication, division;
+    sum = a + b;
+    cout << "SUM: " << sum << endl;
+    difference = a - b;
+    cout << "Difference: " << difference << endl;
+    multiplication = a * b;
+    cout << "Multiplication: " << multiplication << endl;
+    try
+    {
+        division = a / b;
+        cout << "Division: " << division << endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int a, b, op;
+    cout << "Insert first number: ";
+    cin >> a;
+    cout << "Insert second number: ";
+    cin >> b;
+    calculatur(a, b);
+    return EXIT_SUCCESS;
+}
