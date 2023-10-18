e8e7e472673dac0c1c044e246f13e3c69d798dc3
Antonino Cristaldi <uni389908@studium.unict.it>
2023-10-18 18:17:04 +0200

    feat: somma fatta

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,24 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include<iostream>
+using namespace std;
+
+
+int a, b;
+
+
+
+int main(){
+
+  cout<<"Insert the first number"<<endl;
+  cin>>a;
+  cout<<"Insert the second number"<<endl;
+  cin>>b;
+  
+  cout<<"sum: "<<a+b;
+
+  return 1;
+}
+
