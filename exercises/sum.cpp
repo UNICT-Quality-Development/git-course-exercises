f7f2a326f56ab6274b24b5a569401ad1280f9ef9
Biagiocu99 <uni396500@studium.unict.it>
2023-10-18 17:52:07 +0200

    feat: implementazione somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+int sum(x,y){
+return x+y;
+}
+
+int main(){
+int x;
+int y;
+cout<<"inserisci il primo numero:" ;
+cin>>x;
+cout<<endl
+cout<<"inserisci il secondo numero:";
+cin>>y;
+
+cout<<"Il risultato della somma è: "<<sum(x,y)<<end
+}
