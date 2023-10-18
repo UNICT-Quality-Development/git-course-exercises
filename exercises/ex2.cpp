9a8a3fd99fef895dc7bc3556dc66e1c7dedce69a
Fabryx <fabrizio.cipriano@studium.unict.it>
2023-10-18 17:38:03 +0200

    ex 2 with array and switch

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -9,42 +9,37 @@ using namespace std;
 
 int main()
 {
+  string day[8] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Invalid input! Please enter week number between 1-7."};
   int week;
 
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
-
-  if (week == 1)
+  switch (week)
   {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
+  case 1:
+    cout << "day: " << day[0] << endl;
+    break;
+  case 2:
+    cout << "day: " << day[1] << endl;
+    break;
+  case 3:
+    cout << "day: " << day[2] << endl;
+    break;
+  case 4:
+    cout << "day: " << day[3] << endl;
+    break;
+  case 5:
+    cout << "day: " << day[4] << endl;
+    break;
+  case 6:
+    cout << "day: " << day[5] << endl;
+    break;
+  case 7:
+    cout << "day: " << day[6] << endl;
+    break;
+  default:
+    cout << "day: " << day[7] << endl;
+    break;
   }
 
   return 0;
