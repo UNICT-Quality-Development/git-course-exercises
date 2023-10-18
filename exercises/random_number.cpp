2b7e24cd562457317682e44c5bd34a4f0766e375
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-18 17:51:41 +0200

    feat: imolementing random_number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,16 @@
   Output:
   The random number is: 4
 */
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+  srand(time(NULL));
+  int x = rand() % 100;
+
+  cout << "The random number is: " << x << endl;
+}
