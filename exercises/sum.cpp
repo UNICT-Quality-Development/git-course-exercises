1bfcd525bc6c6552261e78880c6ab673a2db6639
Carla0301 <cmasca02@gmail.com>
2023-10-18 18:05:09 +0200

    feat: somma implementata

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,15 @@
   Insert the second number: 2
   Sum: 3
 */
+
+int main(){
+
+	float a, b;
+
+	printf("inserisci due numeri e vedine la somma\n");
+	scanf("%f", &a);
+	scanf("%f", &b);
+
+	printf("la somma e' %f\n", a+b);
+	return;
+}
