0e1cf2b44aaab8c3eaddf1c8bc9281b3deef1bc5
Darakuu <elviost@gmail.com>
2023-10-18 18:18:14 +0200

    feat: added a simple binary converter implementation

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,25 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream> 
+#include <string> 
+ 
+using namespace std; 
+ 
+int main(){ 
+ int n; 
+ string binary_num= ""; 
+  
+ cout<<"Inserisci un numero: "; 
+ cin>>n; 
+ while (n>0) { 
+  if (n%2==0) 
+   binary_num='0'+binary_num; 
+  else 
+   binary_num='1'+binary_num; 
+  n/=2; 
+ } 
+ cout<<"Insert first number: "<< n <<endl;  
+ cout<<"The binary number is: "<< binary_num << endl;  
+}
