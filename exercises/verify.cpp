04d4b2e627e389e429e8130f54cf7ed34ffb3690
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-18 17:37:31 +0200

    feat: verify exercise

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -9,10 +9,17 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
+bool isPresent(int N[], int size, int n) {
+  for(int i = 0; i<size; i++)
+    if(N[i] == n)
+      return true;
+  return false;
+}
+
 int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
-
+  isPresent(N, 10, 3) ? cout << "The number 3 is present in the array." << endl : cout << "The number 3 is not present in the array." << endl;
   return 0;
 }
