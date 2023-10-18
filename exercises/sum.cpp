12551d03ba1fdb25d7c6d0f104cba81df2f12a43
LorenzoMaci <lorenzomaci@virgilio.it>
2023-10-18 17:45:50 +0200

    feat: exercise sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,17 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream> 
+
+using namespace std;
+
+int sum(int a, int b){
+    return a + b;
+}
+
+int main(){
+  int a = 1;
+  int b = 2;
+  cout << "Sum: " <<  sum(a, b) << endl;
+}
