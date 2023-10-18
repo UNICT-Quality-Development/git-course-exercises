62ffe4e7854d11d524157608ba3187b6d55d3bf5
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 22:51:41 +0200

    fix: added spaces and EXIT_SUCCESS

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -11,25 +11,31 @@
   Division: 2
 */
 
+#define EXIT_SUCCESS 0
 #include <iostream>
 
 using namespace std;
 
 int main(){
 
-  double a,b;
-  cout<<"Inserisci due numeri per calcolare somma, differenza, prodotto e divisione"<<endl;
-  cout<<"Primo numero: ";
-  cin>>a;
-  cout<<"Secondo numero: ";
-  cin>>b;
-  cout<<endl;
-  
-  cout<<"Somma: "<<a+b<<endl;
-  cout<<"Differenza: "<<a-b<<endl;
-  cout<<"Prodotto: "<<a*b<<endl;
-  if(b != 0)cout<<"Divisione: "<<a/b<<endl;
-  else cout<<"Divisione: Impossibile Dividere per 0"<<endl;
+  cout << "Inserisci due numeri per calcolare somma, differenza, prodotto e divisione" << endl;
+  cout << "Primo numero: ";  
+  double a;
+  cin >> a;
 
-  return 0;
+  cout << "Secondo numero: ";
+  double b;
+  cin >> b;
+  
+  cout << endl;
+  
+  cout << "Somma: " << a+b << endl;
+  cout<< "Differenza: " << a-b << endl;
+  cout << "Prodotto: " << a*b << endl;
+  if(b != 0) 
+    cout << "Divisione: " << a/b << endl;
+  else 
+    cout << "Divisione: Impossibile Dividere per 0" << endl;
+
+  return EXIT_SUCCESS;
 }
