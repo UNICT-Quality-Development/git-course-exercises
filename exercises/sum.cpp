439cf9e40ec3f0a2b031267c837093f389345a8b
Agostino <agredh29@gmail.com>
2023-10-18 17:37:33 +0200

    feat: implementing sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int sum(int, int);
+
+int main() {
+	int x=1, y=2;
+	
+	cout << x << " + " << y << " = " << sum(x,y) << endl;
+	return 0; 
+}
+
+int sum(int a, int b) {
+	return a + b;
+}
