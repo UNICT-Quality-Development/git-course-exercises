620ad9e443018e1d6cfe7be56c8c3992e9cefbce
linuchs <allegrarex#libero.it>
2023-10-18 18:42:08 +0200

    feat: I solved my first exercise

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,30 @@
   Insert the second number: 2
   Sum: 3
 */
+
+
+
+#include <iostream> 
+using namespace std; 
+  
+
+int somma(int A, int B) 
+{ 
+ 
+    return A + B; 
+} 
+  
+
+int main() 
+{ 
+   
+    int primo;
+    int secondo; 
+     cout << "Inserisci primo: \n";
+      cin >> primo;
+      cout << "Inserisci secondo: \n";
+      cin >> secondo;
+  
+    cout << "Somma = " << somma(primo, secondo); 
+    return 0; 
+}
