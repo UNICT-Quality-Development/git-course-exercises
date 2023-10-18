a98466043f658683d244e21a68e76f084a4c704f
Alessandra Di Fiore <difiore.alessandra93@gmail.com>
2023-10-18 17:51:15 +0200

    chore: implement sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,17 @@
   Insert the second number: 2
   Sum: 3
 */
+#include<iostream>
+ 
+ using namespace std;
+
+ int a, b;
+
+ int main(){
+  cout<<"Insert the first number:";
+  cin>>a;
+  cout<<"Insert the second number:";
+  cin>>b;
+  cout<<"Sum:"<< a+b <<endl;
+  return 1;
+ }
