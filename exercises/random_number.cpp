4e98e3453d7a8940e6e035dd869a613ab774c9b3
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-18 17:56:11 +0200

    feat: random_number exercise done

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,25 @@
   Output:
   The random number is: 4
 */
+
+#define EXIT_SUCCESS 0
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+
+int get_random_number() {
+  return rand();
+}
+
+
+int main(int argc, char *argv[]) {
+  //set the seed for the randomness
+  srand(time(NULL));
+  int random_number = get_random_number();
+  cout << "The random number is: " <<  random_number << endl;
+  return EXIT_SUCCESS;
+}
