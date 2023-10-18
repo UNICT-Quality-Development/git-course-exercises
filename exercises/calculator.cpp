38b681e1b031a750a0d14f7db483acc28a3e4a54
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-18 18:43:53 +0200

    feat: implementing calculator exercise

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,23 @@
   Multiplication: 8
   Division: 2
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  double x, y;
+
+  cout << "Insert first number: ";
+  cin >> x;
+
+  cout << "Insert second number: ";
+  cin >> y;
+
+  cout << "SUM: " << x + y << endl;
+  cout << "Difference: " << x - y << endl;
+  cout << "Multiplication: " << x * y << endl;
+  cout << "Division: " << x / y << endl;
+  
+  return 0;
+}
