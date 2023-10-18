40ebbae30f75e8aa8cb5751f243689f1eea8c043
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 22:56:08 +0200

    fix: added spaces and EXIT_SUCCESS

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -6,21 +6,30 @@ Insert number 3
 The number 3 is [not] present in the array.
 */
 
+#define EXIT_SUCCESS 0
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+
   bool isinarray = false;
+
+  cout << "Inserisci numero: ";
   int numToCheck;
-  cout<<"Inserisci numero: ";
-  cin>>numToCheck;
-  for(int i=0;i<10;i++){
+  cin >> numToCheck;
+
+  for(int i=0; i<10; i++){
+
     if(N[i] == numToCheck) isinarray = true;
+
   }
-  if(isinarray) cout<<"Il numero "<<numToCheck<<" si trova nell'array"<<endl;
-  else cout<<"Il numero "<<numToCheck<<" non si trova nell'array"<<endl;
-  return 0;
+
+  if(isinarray) cout << "Il numero " << numToCheck << " si trova nell'array" << endl;
+  else cout << "Il numero " << numToCheck << " non si trova nell'array" << endl;
+
+  return EXIT_SUCCESS;
 }
