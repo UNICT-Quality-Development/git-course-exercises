85ce8df4b0255715265adacc6dd710b69f274413
MarcoRannisi <x81000391@studium.unict.it>
2023-10-18 17:51:56 +0200

    feat:sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,21 @@
   Insert the second number: 2
   Sum: 3
 */
+
+
+#include <stdio.h>
+
+int sum(int a, int b){
+  return a+b;
+}
+
+int main(int argc, char const *argv[]){
+  int a, b;
+  printf("inserire due numeri:\n");
+  scanf("%d", &a);
+  scanf("%d", &b);
+  printf("La somma restituira è: %d\n", sum(a,b));
+
+  return 0;
+}
+
