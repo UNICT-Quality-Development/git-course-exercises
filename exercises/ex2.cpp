a1083e23d64c64df51237ec8d6978648f6eb4eb1
Gianluca Di Paola <gianlucadipaola18@gmail.com>
2023-10-18 18:46:11 +0200

    Chore: minor improvements

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -11,41 +11,18 @@ int main()
 {
   int week;
 
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+  while(true){
+    cout << "Enter week number(1-7): " << endl;
+    cin >> week;
+    if(week < 1 || week > 7)
+      cerr << "Wrong week number, please insert a valid one. (1-7)" << endl;
+    else
+      break;
+  }
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+  string A[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+  cout << A[week-1];
 
   return 0;
 }
