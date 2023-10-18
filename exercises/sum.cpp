9b971e98052462bff37883965242e03900ad1c40
Irial McKeown <irialcp@gmail.com>
2023-10-18 18:22:50 +0200

    feat: sum fuction

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+void main(void){
+  int a, b;
+  cout << "insert the first number:";
+  cin >> a;
+  cout << "insert the second number:";
+  cin >> b;
+  int sum = a +b;
+  cout << "Sum: %d", sum;
+}
