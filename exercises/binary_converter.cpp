8e249f2392eb0f675bb780d4cbff18240f1a97e1
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 22:48:07 +0200

    fix:added EXIT_SUCCESS

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -6,27 +6,31 @@
   The binary number is: 1000
 */
 
+#define EXIT_SUCCESS 0
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 string reverseString(string s){
+
   string a = s;
-  for(int i = 0; i < s.length(); i++) a[i] = s[(s.length()-1)-i];
+  for(int i = 0; i < s.length(); i++) a[i] = s[(s.length() - 1) - i];
   return a;
+
 }
 
 int main(){
-  int numberToConvert;
   cout << "Inserisci numero da convertire: ";
+  int numberToConvert;
   cin >> numberToConvert;
 
   string binary = "";
   while (numberToConvert >= 1){
     binary += ((numberToConvert % 2 == 1) ? "1" : "0");
-    numberToConvert/=2;
+    numberToConvert /= 2;
   }
   cout << "Il numero in binario e\': "<< reverseString(binary) << endl;
-  return 0;
+  return EXIT_SUCCESS;
 }
