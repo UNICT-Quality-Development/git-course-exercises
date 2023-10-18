9df21372fb4778857543cd799bf1aba6b1d0b2f2
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-18 18:28:52 +0200

    feat: implementing verify exercise

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -9,10 +9,29 @@ The number 3 is [not] present in the array.
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
+  int x;
+  cout << "Insert a number: ";
+  cin >> x;
+
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
+  bool present = false;
+
+  for(int i = 0; i<10; i++){
+    if(N[i] == x){
+      present = true;
+      break;
+    }
+  }
+
+  if(present == true){
+    cout << "The number " << x << " is present in the array." << endl;
+  }
+  else{
+    cout << "The number " << x << " is [not] present in the array." << endl;
+  }
+  
   return 0;
 }
