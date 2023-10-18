22ff585af188f2bbaf6e09a43499a464bff2dcdf
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-18 18:37:22 +0200

    feat: binary converter exercise

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,28 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#define EXIT_SUCCESS
+#include <iostream>
+
+using namespace std;
+
+string to_binary(int n)
+{
+    string binary = "";
+    while (n > 0)
+    {
+        binary = (n % 2 == 0 ? "0" : "1") + binary;
+        n /= 2;
+    }
+    return binary;
+}
+
+int main(int argc, char *argv[])
+{
+    int n;
+    cout << "Insert first number: ";
+    cin >> n;
+    cout << "The binary number is: " << to_binary(n) << endl;
+    return EXIT_SUCCESS;
+}
