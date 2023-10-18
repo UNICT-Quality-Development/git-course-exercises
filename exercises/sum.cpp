9a4bdda1cb12ebc2da56bb73c0b4e0c8e2c35026
FilippoMarletta <filippo.marletta@ctmobi.it>
2023-10-18 17:26:09 +0200

    feat(sum.cpp): implemented sum function

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,22 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b){
+  return a + b;
+}
+
+int main(){
+  int a, b;
+  cout << "Inserire 2 numeri da sommare" << endl;
+  cout << "primo numero:" << endl;  
+  cin >> a; 
+  cout << "secondo numero:" << endl;
+  cin >> b;  
+  cout << "= " << sum(a, b) << endl;
+
+  return EXIT_SUCCESS; 
+}
