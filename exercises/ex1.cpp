b3f5754fddd4dfd0d54d436c344f7000d0c9206b
MarcoRannisi <x81000391@studium.unict.it>
2023-10-18 18:10:23 +0200

    feat:ex1.cpp

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -3,46 +3,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int week;
+int main(){
+  int day;
+  string week[]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
 
   /* Input week number from user */
+  
   cout << "Enter week number(1-7): " << endl;
-  cin >> week;
-
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
-
+  cin >> day;
+  if(day >0 && day < 8)cout << week[day -1]<< endl;
+  else cout << "Invalid input! Please enter week number between 1-7." << endl;
   return 0;
 }
