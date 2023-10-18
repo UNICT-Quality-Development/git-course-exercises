8afd98c50fb67364aecb36db187f5060ac85ee78
Henrysk <henriskdusk@gmail.com>
2023-10-18 18:55:08 +0200

    feat: Add an endline when printing a number in binary

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -6,7 +6,7 @@
   The binary number is: 1000
 */
 #include <iostream>
-void printNumberBinary(unsigned long long int n)
+void __printNumberBinary(unsigned long long int n)
 {
   //Checks if we are in the base case
   if(n<=1)
@@ -16,15 +16,20 @@ void printNumberBinary(unsigned long long int n)
     return;
   }
   //Recursion happens here
-  printNumberBinary(n/2);
+  __printNumberBinary(n/2);
   //Print the last bit
   std::cout << n%2;
 }
+void printNumberBinary(unsigned long long int n)
+{
+  __printNumberBinary(n);
+  std::cout << std::endl;
+}
+
 int main()
 {
   std::cout << "Input the number to convert to base 2" << std::endl;
   unsigned long long m=0;
   std::cin >> m;
   printNumberBinary(m);
-  std::cout << std::endl;
 }
