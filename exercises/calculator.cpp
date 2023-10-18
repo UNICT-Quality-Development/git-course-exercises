d965695cc76931747b0f9723131e4e3b1223916b
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 18:28:23 +0200

    feat: implented calculator exercise

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,26 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  double a,b;
+  cout<<"Inserisci due numeri per calcolare somma, differenza, prodotto e divisione"<<endl;
+  cout<<"Primo numero: ";
+  cin>>a;
+  cout<<"Secondo numero: ";
+  cin>>b;
+  cout<<endl;
+  
+  cout<<"Somma: "<<a+b<<endl;
+  cout<<"Differenza: "<<a-b<<endl;
+  cout<<"Prodotto: "<<a*b<<endl;
+  if(b != 0)cout<<"Divisione: "<<a/b<<endl;
+  else cout<<"Divisione: Impossibile Dividere per 0"<<endl;
+
+  return 0;
+}
