2b3548667651cad59f80a0f78a5bf6975ff203d4
Riccardo Rubino <Ricky.Rubino@yahoo.it>
2023-10-18 18:00:23 +0200

    feat: added array of days to avoid using switch statement

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -5,47 +5,21 @@
 */
 
 #include <iostream>
+#include <string>
+#define EXIT_SUCCESS 0
+
 using namespace std;
 
 int main()
 {
+  string days[7] = {"Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday", "Sunday"};
   int week;
 
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+  if (week > 7 || week < 1) cout << "Invalid input! Please enter week number between 1-7." << endl;
+  else cout << days[week - 1] << endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
