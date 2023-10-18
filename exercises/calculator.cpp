5d9653247a4d797ec694e86749ae4885abdad453
Carla0301 <cmasca02@gmail.com>
2023-10-18 18:33:17 +0200

    feat: calcolatrice implementata

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,21 @@
   Multiplication: 8
   Division: 2
 */
+
+int main() {
+
+	float a, b;
+
+	printf("inserisci due numeri e ottieni la loro somma, sottrazione, moltiplicazione e divisione\n");
+
+	printf("inserisci il primo numero\n");
+	scanf("%f", &a);
+	printf("inserisci il secondo numero\n");
+	scanf("%f", &b);
+
+	printf("la loro somma e' %f\n", a + b);
+	prinf("la loro differenza e' %f\n", a - b);
+	printf("il loro prodotto e' %f\n", a * b);
+	if (b == 0) printf("non e' possibile dividere per 0\n");
+	else printf("la loro divisione e' %f\n", a / b);
+}
