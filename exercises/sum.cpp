6b169cb525336323fb7c4bdc4623e70c335ba494
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-18 17:36:37 +0200

    feat: sum done

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,15 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b, sum;
+	cout << "insert the first number:";
+	cin >> a;
+	cout << "insert the second number:";
+	cin >> b;
+	sum = a+b;
+	cout << "Sum:" << sum;
+}
