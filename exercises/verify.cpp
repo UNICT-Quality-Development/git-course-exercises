2dddf85cbb307bca97e8bb17afd3f5b3c026839b
Andrysora <andrea.seminara01@gmail.com>
2023-10-18 17:59:44 +0200

    feat:implemented verify exercise

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -13,6 +13,14 @@ int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
-
+  bool isinarray = false;
+  int numToCheck;
+  cout<<"Inserisci numero: ";
+  cin>>numToCheck;
+  for(int i=0;i<10;i++){
+    if(N[i] == numToCheck) isinarray = true;
+  }
+  if(isinarray) cout<<"Il numero "<<numToCheck<<" si trova nell'array"<<endl;
+  else cout<<"Il numero "<<numToCheck<<" non si trova nell'array"<<endl;
   return 0;
 }
