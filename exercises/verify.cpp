61bc43f8a9605c8e422551950c3f2f0c070627e2
Riccardo Rubino <Ricky.Rubino@yahoo.it>
2023-10-18 18:45:52 +0200

    feat: implemented isPresent function and verification

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -7,12 +7,25 @@ The number 3 is [not] present in the array.
 */
 
 #include <iostream>
+#define EXIT_SUCCESS 0
 using namespace std;
+
+bool isPresent(int* arr, int dim, int num){
+  for (int i = 0; i < dim; i++) if (arr[i] == num) return true;
+  return false;
+}
 
 int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int arr_length = sizeof(N)/sizeof(int);
+  int input_number;
 
-  return 0;
+  cout << "Insert number ";
+  cin >> input_number;
+
+  cout << "The number " << input_number << " is" << ((isPresent(N, arr_length, input_number)) ? " " : " not ") << "present in the array." << endl;
+
+  return EXIT_SUCCESS;
 }
