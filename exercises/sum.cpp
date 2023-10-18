54ecfddac875bb7a5b4b77c04bdc9466c9c8b35e
ZENZE989 <claudioterranova@studium.unict.it>
2023-10-18 17:55:35 +0200

    first_v

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,24 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char s[80];
+	int a, b, SUM;
+	fget(s, sizeof(s), stdin);
+	a = atoi(s);
+
+	fget(s, sizeof(s), stdin);
+	b = atoi(s);
+
+	SUM = a + b;
+
+	printf("la somma è %i"\n, SUM);
+
+	RETURN 0;
+}
+
