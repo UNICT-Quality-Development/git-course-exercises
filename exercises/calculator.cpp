8746aea4c3b8012d3502d55912aad15dbbc3ba77
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-18 18:02:19 +0200

    feat: calculator done

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,27 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b, result;
+	cout << "insert first number:";
+	cin >> a;
+	cout << "insert second number:";
+	cin >> b;
+	result=a+b;
+	cout << "SUM: " << result;
+	cout << "\n";
+        result=a-b;
+        cout << "Difference: " << result;
+        cout << "\n";
+        result=a*b;
+        cout << "Multiplication: " << result;
+        cout << "\n";
+        result=a/b;
+        cout << "Division: " << result;
+        cout << "\n";
+	return 0;
+}
+
