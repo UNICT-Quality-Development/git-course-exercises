220ca33db4babe0ecab9e54d4fc13dd600ea743a
Fabryx <fabrizio.cipriano@studium.unict.it>
2023-10-19 12:41:34 +0200

    Here i use the map

--- exercises/ex3.cpp
+++ exercises/ex3.cpp
@@ -1,43 +1,37 @@
 /* Could you still use a switch case here? May you can use a map. */
 
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main()
 {
+  map<string,string> famous;
   string textInput;
+  famous["BarackObama"]="44th president of the United States";
+  famous["SandroPertini"]="Former President of the Italian Republic";
+  famous["NelsonMandela"]="Former President of South Africa";
+  famous["MahatmaGandhi"]="Bapu";
+  famous["DonaldKnuth"]="Creator of LaTeX";
+  famous["DennisRitchie"]="Creator of C";
 
   cout << "Enter a famous name+surname, ex. BarackObama " << endl;
   cin >> textInput;
-
-  if (textInput == "BarackObama")
+  bool flag=true;
+for (auto i = famous.begin(); i != famous.end(); ++i)
+{
+  if(i->first==textInput)
   {
-    cout << "44th president of the United States" << endl;
+    cout<<i->first<<" Description: "<<i->second<<endl;
+    flag=false;
+    break;
+    }
+  
+}
+  if (flag!=false)
+  {cout << "Invalid input! Please enter a good name!" << endl;
   }
-  else if (textInput == "SandroPertini")
-  {
-    cout << "Former President of the Italian Republic" << endl;
-  }
-  else if (textInput == "NelsonMandela")
-  {
-    cout << "Former President of South Africa" << endl;
-  }
-  else if (textInput == "MahatmaGandhi")
-  {
-    cout << "Bapu" << endl;
-  }
-  else if (textInput == "DonaldKnuth")
-  {
-    cout << "Creator of LaTeX" << endl;
-  }
-  else if (textInput == "DennisRitchie")
-  {
-    cout << "Creator of C" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter a good name!" << endl;
-  }
+  
 
   return 0;
 }
