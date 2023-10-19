705a89cf7eed17758907b8dc1968a1768bafd306
Fabryx <fabrizio.cipriano@studium.unict.it>
2023-10-19 07:01:39 +0200

    piccole sistemazioni

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -7,15 +7,23 @@
   Sum: 3
 */
 #include<iostream>
+
 using namespace std;
 
 int main()
 {
   int num1=0,num2=0,sum=0;
+
   cout<<"Inserisci il primo numero: ",cin>>num1;
+
   cout<<"Inserisci il secondo numero: ",cin>>num2;
+
   sum=num1+num2;
+
   cout<<"Ecco la somma dei numeri inseriti: "<<sum<<endl;
+
+
+  return 0;
 
 
 }
