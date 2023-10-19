655cef3e91b9eda011bc72d869364ddc31cd61aa
Fabryx <fabrizio.cipriano@studium.unict.it>
2023-10-19 12:42:51 +0200

    Here i use the map

--- exercises/ex3.cpp
+++ exercises/ex3.cpp
@@ -1,37 +1,35 @@
 /* Could you still use a switch case here? May you can use a map. */
-
 #include <iostream>
 #include <map>
 using namespace std;
 
 int main()
 {
-  map<string,string> famous;
+  map<string, string> famous;
   string textInput;
-  famous["BarackObama"]="44th president of the United States";
-  famous["SandroPertini"]="Former President of the Italian Republic";
-  famous["NelsonMandela"]="Former President of South Africa";
-  famous["MahatmaGandhi"]="Bapu";
-  famous["DonaldKnuth"]="Creator of LaTeX";
-  famous["DennisRitchie"]="Creator of C";
+  famous["BarackObama"] = "44th president of the United States";
+  famous["SandroPertini"] = "Former President of the Italian Republic";
+  famous["NelsonMandela"] = "Former President of South Africa";
+  famous["MahatmaGandhi"] = "Bapu";
+  famous["DonaldKnuth"] = "Creator of LaTeX";
+  famous["DennisRitchie"] = "Creator of C";
 
   cout << "Enter a famous name+surname, ex. BarackObama " << endl;
   cin >> textInput;
-  bool flag=true;
-for (auto i = famous.begin(); i != famous.end(); ++i)
-{
-  if(i->first==textInput)
+  bool flag = true;
+  for (auto i = famous.begin(); i != famous.end(); ++i)
   {
-    cout<<i->first<<" Description: "<<i->second<<endl;
-    flag=false;
-    break;
+    if (i->first == textInput)
+    {
+      cout << i->first << " Description: " << i->second << endl;
+      flag = false;
+      break;
     }
-  
-}
-  if (flag!=false)
-  {cout << "Invalid input! Please enter a good name!" << endl;
   }
-  
+  if (flag != false)
+  {
+    cout << "Invalid input! Please enter a good name!" << endl;
+  }
 
   return 0;
 }
