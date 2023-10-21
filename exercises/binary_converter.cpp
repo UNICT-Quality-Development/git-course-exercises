14d3d9ed41eb46cb808c4f9953208362ad9b622d
linuchs <allegrarex#libero.it>
2023-10-21 20:17:56 +0200

    feat: implementing binary_converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,26 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+  int first;
+  int binary[16]; 
+
+  cout << "Insert the first number: ";
+  cin >> first;
+  int i = 0; 
+  while (first > 0) {
+    binary[i] = first % 2; 
+    first = first / 2; 
+    i++; 
+  } 
+  
+  cout <<"The binary number is: "; 
+  for (int j = i - 1; j >= 0; j--) 
+    cout << binary[j]; 
+  return 0; 
+} 
