331b436c01952bd6d28ce9229b558c71fd08025d
linuchs <allegrarex#libero.it>
2023-10-21 23:42:41 +0200

    feat: This is my calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,19 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+
+using namespace std;
+int main() {
+  int first;
+  int second;
+  cout << "Insert the first number: ";
+  cin >> first;
+  cout << "Insert the second number: ";
+  cin >> second;
+  cout << "SUM: " << first + second << "\n";
+  cout << "Difference: " << first - second << "\n";
+  cout << "Multiplication: " << first * second << "\n";
+  cout << "Division: " << first / second;
+  return 0;
+}
