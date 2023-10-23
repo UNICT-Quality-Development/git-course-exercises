63b438097ae4d748cad58edd37797a018aa380d2
ZENZE989 <claudioterranova@studium.unict.it>
2023-10-23 16:38:52 +0200

    complete

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -9,4 +9,23 @@
   Difference: 2
   Multiplication: 8
   Division: 2
-*/
+*/ 
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	int a, b, dif,sum ;
+	
+	
+	a = 4;
+	b = 2;
+	
+	sum = a+b;
+	dif= a-b;
+	
+	printf("\n Insert first number:%i\n Insert first number:%i\n SUM: %i\n Difference: %i\n Multiplication: %i\n Division: %i\n", a, b, sum,  dif, a*b, a/b); 
+	
+	return 0;
+}	
+	
