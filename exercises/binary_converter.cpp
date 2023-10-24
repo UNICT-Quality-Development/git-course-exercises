50539503b1bcdc510834793169c7e420a5f82ef3
ZENZE989 <claudioterranova@studium.unict.it>
2023-10-24 11:22:01 +0200

    completed

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,27 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+
+
+int main(){
+	
+	int s[80];
+	int a;
+	
+	a = 8;
+	
+	while (a/2 != 0){
+		
+		a%2 = s;
+		
+	}
+	
+	printf ("%i" ,s);
+	
+}
+	
+	
+	
+	
+	
