27220718be65505027c99d3d77759c049b56a953
Giovanni Pinelli <giovannipinelli04@gmail.com>
2023-10-30 18:19:26 +0100

    feat: random-number implemented

---
+++ exercises/random_number.py
@@ -0,0 +1,12 @@
+"""""""""""""""
+  Write a program that generates a random number.
+
+  Output:
+  The random number is: 4
+
+"""""""""""""""
+import random
+
+x = random.randint(1,10)
+
+print(" The random number is:", x)
