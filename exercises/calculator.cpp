0fd837d6a608f26fe7bde7a4b905493f741d81ba
Giulio Taccetta <giulio.taccetta@gmail.com>
2025-10-30 17:49:42 +0100

    add feat(calculator)

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,22 @@
   Multiplication: 8
   Division: 2
 */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+	cout<<"inserisci il primo valore"<<endl;
+	cin>>x;
+	cout<<"inserisci il seocndo valore"<<endl;
+	cin>>y;
+
+	cout<<"Somma: "<<x+y<<endl;
+	cout<<"Differenza: "<<x-y<<endl;
+	cout<<"Divisione: "<<x/y<<endl;
+	cout<<"Prodotto: "<<x*y<<endl;
+
+}
