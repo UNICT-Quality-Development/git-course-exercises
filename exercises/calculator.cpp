3a937445883399a89893c954a2b082b23c8b7268
enzo <enzo13617@gmail.com>
2025-10-30 18:46:40 +0100

    feat (calculator.cpp): exercise done

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,24 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+#define EXIT_SUCCESS 0
+
+using namespace std;
+
+int main(){
+
+  double x, y;
+
+  do{
+  cout << "Inserisci due numeri: ";
+  cin >> x >> y;
+  }while(y == 0);
+
+  cout << "SUM: " << x + y <<endl;
+  cout << "Difference: " << x - y <<endl;
+  cout << "Multiplication: " << x * y <<endl;
+  cout << "Division: " << x / y <<endl;
+
+  return EXIT_SUCCESS;
+}
