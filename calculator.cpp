cef123f06e6677bca31264c8e613306901a303d5
Daniele Conti <conti.daniele.220406@gmail.com>
2025-10-30 18:44:25 +0100

    fix: removed wrong files + updated code

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,37 @@
-// A program that given two numbers as input make the main operations.
+// A program that given two numbers as input makes the main operations.
 
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
+//Function sum
+int sum(int x, int y){
+    return x+y;
+}
+
+//Function product
+int product(int x, int y){
+    return x*y;
+}
+
+//Function subtraction
+int subtraction(int x, int y){
+    return x-y;
+}
+
+//Function division
+int division(int x, int y){
+    return x/y; //ik it doesn't return the rest of the division... can't be bothered for a practice exercise tbh
+}
+
+//Main
 int main(){
     int x;
     int y;
     cout << "Enter two digit numbers: " << endl;
     cin >> x >> y;
-    cout << endl << "Sum: " << x << " + " << y << " = " << x+y << endl;
-    cout << "Product: " << x << " x " << y << " = " << x*y << endl;
-    cout << "Subtraction: " << x << " - " << y << " = " << x-y << endl;
-    cout << "Division: " << x << " / " << y << " = " << x/y << endl;
+    cout << endl << "Sum: " << x << " + " << y << " = " << sum(x, y) << endl;
+    cout << "Product: " << x << " x " << y << " = " << product(x, y) << endl;
+    cout << "Subtraction: " << x << " - " << y << " = " << subtraction(x, y) << endl;
+    cout << "Division: " << x << " / " << y << " = " << division(x, y) << endl;
     return 0;
 }
