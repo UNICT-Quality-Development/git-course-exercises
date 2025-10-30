4c6999253b0bc9607ba2af3f10672bcb20b93231
Nathh17 <ultrazocker21@gmail.com>
2025-10-30 18:05:04 +0100

    refactor: Added switch-case in function

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -3,6 +3,35 @@
 #include <iostream>
 using namespace std;
 
+void printDay(int week){
+  switch(week){
+    case 1:
+      cout<<"Monday"<<endl;
+      break;
+    case 2:
+      cout<<"Tuesday"<<endl;
+      break;
+    case 3:
+      cout<<"Wednesday"<<endl;
+      break;
+    case 4:
+      cout<<"Thursday"<<endl;
+      break;
+    case 5:
+      cout<<"Friday"<<endl;
+      break;
+    case 6:
+      cout<<"Saturday"<<endl;
+      break;
+    case 7:
+      cout<<"Sunday"<<endl;
+      break;
+    default:
+      cout<<"Error: Inser a number between 1-7"<<endl;
+      break;
+  }
+}
+
 int main()
 {
   int week;
@@ -11,32 +40,7 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  switch(week){
-    case 1:
-      cout<<"Monday"<<endl;
-    break;
-    case 2:
-      cout<<"Tuesday"<<endl;
-    break;
-    case 3:
-      cout<<"Wednesday"<<endl;
-    break;
-    case 4:
-      cout<<"Thursday"<<endl;
-    break;
-    case 5:
-      cout<<"Friday"<<endl;
-    break;
-    case 6:
-      cout<<"Saturday"<<endl;
-    break;
-    case 7:
-      cout<<"Sunday"<<endl;
-    break;
-    default:
-      cout<<"Error: Inser a number between 1-7"<<endl
-    break;
-  }
+  printDay(week);
 
   return 0;
 }
