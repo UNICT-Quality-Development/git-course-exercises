54a789eabd20a9ccb472e2c2edecccdbb84cc125
Antonio Pistone <uniantoniopistone@gmail.com>
2025-10-30 17:37:31 +0100

    feat: created sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,15 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+int main(){
+	int n1,n2,somma;
+	cout<<"Insert the first number:";
+	cin>>n1;
+        cout<<"Insert the second number:";
+        cin>>n2;
+	somma=n1+n2;
+	cout<<"Sum:"<<somma<<endl;
+	
+}
