8485de46ef843bf91e6a56d3b6324809044b3d92
Daniele Conti <conti.daniele.220406@gmail.com>
2025-10-30 17:21:40 +0100

    feat: added file.cpp

---
+++ file.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    double x;
+    double y;
+
+    cout << "Enter first digit: " << endl;
+    cin >> x;
+    cout << "Enter second digit: " << endl;
+    cin >> y;
+    cout << "The sum of " << x << " and " << y << "is: " << x+y << endl;
+    return 0;
+}
