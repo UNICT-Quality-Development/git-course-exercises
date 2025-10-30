5bcda2336dd1316fd3b31bde9212651ee20c294c
Francesco <94438481+Ciccio1307@users.noreply.github.com>
2025-10-30 17:42:01 +0100

    feat:verifyCreated

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -15,4 +15,21 @@ int main()
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
   return 0;
+
+
+  cout << "Insert number: ";
+  int num;
+  cin >> num;
+  bool found = false;
+  for (int i = 0; i < 10; i++) {
+    if (N[i] == num) {
+      found = true; 
+}
+  }
+  if (found) {
+    cout << "The number " << num << " is present in the array." << endl;
+  } else {
+    cout << "The number " << num << " is not present in the array." << endl;
+  }
+  return 0;
 }
