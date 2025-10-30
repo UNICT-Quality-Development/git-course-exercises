4291b2c155f93989333a30c91481ee301b6d7321
Nathh17 <ultrazocker21@gmail.com>
2025-10-30 23:17:57 +0100

    feat(binary_converter.cpp): Implemented Recursive Binary Converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,27 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+
+using namespace std;
+
+int binaryConverter(int x){
+  if(x<0){
+    cout<<"Inserire Numero Positivo";
+    return -1;
+  }
+
+  if(x==0)
+    return 0;
+  else
+    return (x%2 + 10*binaryConverter(x/2));
+}
+
+int main(int argc, char* argv[]){
+  if(argc != 2){
+    cout<<"Usage: ./a.out <Num:int>"<<endl;
+    return -1;
+  }
+  cout<<binaryConverter(atoi(argv[1]))<<endl;
+}
