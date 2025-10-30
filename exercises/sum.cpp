b2d5de881f9cd92d6be62f34269e9159391d9398
Vlitraxes <gabriele.malox@gmail.com>
2025-10-30 17:29:08 +0100

    Esercizio sum completato

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,24 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b){return a + b;}
+
+int main(){
+
+int a,b;
+
+cout << "Inserire 2 interi differenziati da uno spazio" << endl;
+cin >> a >> b;
+
+int AddUp = sum(a,b);
+
+cout << "Il risultato di questa addizzione e' " << AddUp << endl;
+
+return 0;
+
+
+}
