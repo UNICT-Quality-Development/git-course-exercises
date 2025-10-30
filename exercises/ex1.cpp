eca2877232d72331acec054e7bdd8260050c222f
Marco <intensework443@gmail.com>
2025-10-30 17:46:17 +0100

    chore: cambiato if con switchù

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -11,6 +11,34 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
+  switch(week)
+  {
+    case 1:
+      cout << "Monday" << endl;
+      break;
+    case 2:
+      cout << "Tuesday" << endl;
+      break;
+    case 3:
+      cout << "Wednesday" << endl;
+      break;
+    case 4:
+      cout << "Thursday" << endl;
+      break;
+    case 5:
+      cout << "Friday" << endl;
+      break;
+    case 6:
+      cout << "Saturday" << endl;
+      break;
+    case 7:
+      cout << "Sunday" << endl;
+      break;
+    default:
+      cout << "Invalid input! Please enter week number between 1-7." << endl;
+  }
+
+  /*
   if (week == 1)
   {
     cout << "Monday" << endl;
@@ -43,6 +71,6 @@ int main()
   {
     cout << "Invalid input! Please enter week number between 1-7." << endl;
   }
-
+  */
   return 0;
 }
