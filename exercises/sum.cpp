f22a66d5813b04ebed20766c1d33c773054aef70
giuliopedicone02 <pediconegiulio02@gmail.com>
2025-10-30 17:26:12 +0100

    feat: esercizio somma

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,23 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double num1 = 0, num2 = 0;
+
+  cout << "Insert the first number: ";
+  cin >> num1;
+
+  cout << "Insert the second number: ";
+  cin >> num2;
+
+  double sum = num1 + num2;
+
+  cout << "Sum: " << sum << endl;
+
+  return 0;
+}
