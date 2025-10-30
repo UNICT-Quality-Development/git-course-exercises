45f9f4ca7e3096db9d3441603421c6a660625bc0
AvaiaMbare98 <giograsso98@gmail.com>
2025-10-30 18:37:32 +0100

    feat: implemented random number generator

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,16 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <cstdlib>
+
+int numGenerator(){
+  srand(time(NULL));
+  return rand() % 100 + 1;
+}
+
+int main(){
+  std::cout << "A random number between 1 and 100: " << numGenerator() << std::endl;
+  return 0;
+}
