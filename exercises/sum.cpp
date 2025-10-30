273b78cf7c679aa1704efa46e6ba2fd257af20d3
Francesco <94438481+Ciccio1307@users.noreply.github.com>
2025-10-30 17:23:39 +0100

    feat:Sum Created by Francesco Prospero Antonio VIRZI

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+int main() {
+  double num1, num2;
+
+  cout << "Insert the first number:";
+  cin >> num1;
+
+  cout << "Insert the second number: ";
+  cin >> num2;
+
+  cout << "\nSum: " << (num1 + num2) << endl;
+
+
+  return 0;
+}
