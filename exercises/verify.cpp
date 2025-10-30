af7a2c3cc2ed671ba6b4a01bde13a6c2cdaba9e1
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-10-30 18:36:35 +0100

    feat(verify): add verify program.

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -7,12 +7,34 @@ The number 3 is [not] present in the array.
 */
 
 #include <iostream>
+
 using namespace std;
+
+bool checkNum(int* N, int num){
+
+  for(int i=0;i<sizeof(N);i++){
+
+    if(num==N[i]){
+      return true;
+    }
+  }
+  return false;
+}
 
 int main()
 {
-  // placeholder
+  int number;
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+
+  cout<<"Insert number: ";
+  cin>>number;
+  
+  if(checkNum(N,number)){
+    cout<<"The number "<<number<<" is present in the array."<<endl;
+  }
+  else{
+    cout<<"The number "<<number<<" is not present in the array."<<endl;
+  }
 
   return 0;
 }
