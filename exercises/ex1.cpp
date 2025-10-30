ef7aee621da39ba5472794e678e9d1649ab3aa34
Nathh17 <ultrazocker21@gmail.com>
2025-10-30 17:45:22 +0100

    feat: Added expected output

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -13,27 +13,29 @@ int main()
 
   switch(week){
     case 1:
-
+      cout<<"Monday"<<endl;
     break;
     case 2:
-
+      cout<<"Tuesday"<<endl;
     break;
     case 3:
-
+      cout<<"Wednesday"<<endl;
     break;
     case 4:
-
+      cout<<"Thursday"<<endl;
     break;
     case 5:
-
+      cout<<"Friday"<<endl;
     break;
     case 6:
-
+      cout<<"Saturday"<<endl;
     break;
     case 7:
-
+      cout<<"Sunday"<<endl;
     break;
-
+    default:
+      cout<<"Error: Inser a number between 1-7"<<endl
+    break;
   }
 
   return 0;
