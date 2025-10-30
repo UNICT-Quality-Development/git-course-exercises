a5c4897a96dd600827a603e80d3f34a9bef894aa
Davide Avellino <d06.avellino@gmail.com>
2025-10-30 18:11:13 +0100

    fix: I only know c

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -9,4 +9,16 @@
   Difference: 2
   Multiplication: 8
   Division: 2
-*/
+  */
+
+
+#include <stdio.h> 
+
+int main(){
+	int x;
+	int y;
+	printf("\ninsert x and y: ");
+	scanf("%d %d",&x,&y);
+	
+	printf("sum: %d   diff: %d  multiplct: %d   div: %d",(x+y), (x-y), (x*y), (x/y));
+}
