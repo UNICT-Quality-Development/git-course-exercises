80e8774bf8b5413cf2b8ea635e2f61fecd4bfc9f
Andrea Aprile <andy@LenovoAndrea.localdomain>
2025-10-30 18:01:44 +0100

    fix the error

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -12,13 +12,13 @@
 using namespace std;
 int main()
 {
-	int n1,n2,sum;
-	cout<<"Enter a number: ";
-	cin>>n1;
-	cout<<"Enter a number: ";
-	cin>>n2;
-	sum = n1 + n2;
-	cout<<"The sum is: "<<sum;
+ int n1,n2,sum;
+ cout<<"Enter a number: ";
+ cin>>n1;
+ cout<<"Enter a number: ";
+ cin>>n2;
+ sum = n1 + n2;
+ cout<<"The sum is: "<<sum;
 	
 	
 }
