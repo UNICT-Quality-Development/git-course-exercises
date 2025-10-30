3955568a4ee434693768a3e0b47be2a96bc594f0
Daniele Mannino <danymannino@gmail.com>
2025-10-30 17:59:49 +0100

    feat: upgraded code with switch

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -11,38 +11,38 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  switch (week): {
-  case 1:
+  if (week == 1)
+  {
     cout << "Monday" << endl;
-    break;
-  
-  case 2:
+  }
+  else if (week == 2)
+  {
     cout << "Tuesday" << endl;
-    break;
-  
-  case 3:
+  }
+  else if (week == 3)
+  {
     cout << "Wednesday" << endl;
-    break;
-  
-  case 4:
+  }
+  else if (week == 4)
+  {
     cout << "Thursday" << endl;
-    break;
-  
-  case 5:
+  }
+  else if (week == 5)
+  {
     cout << "Friday" << endl;
-    break;
-  
-  case 6:
+  }
+  else if (week == 6)
+  {
     cout << "Saturday" << endl;
-    break;
-
-  case 7:
+  }
+  else if (week == 7)
+  {
     cout << "Sunday" << endl;
-    break;
-
-  default:
+  }
+  else
+  {
     cout << "Invalid input! Please enter week number between 1-7." << endl;
-    break;
-  }   
+  }
+
   return 0;
 }
