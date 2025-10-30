d7b0c3a534c1af97ca7aafb7bfd6b457c384f930
giuliopedicone02 <pediconegiulio02@gmail.com>
2025-10-30 17:43:33 +0100

    feat: add verify.cpp

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -14,5 +14,21 @@ int main()
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
+  int number = 0;
+
+  cout << "Insert number ";
+  cin >> number;
+
+  for (int i = 0; i < 10; i++)
+  {
+    if (N[i] == i)
+    {
+      cout << "The number " << number << "is present in the array.";
+      return 1;
+    }
+  }
+
+  cout << "The number " << number << " is not present in the array." << endl;
+  return 1;
   return 0;
 }
