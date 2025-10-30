29a591792574199e0fde8221c5cc0b388e4e41a9
MargaroneGiada <mrggrs01r62a494a@studium.unict.it>
2025-10-30 17:29:12 +0100

    feat: implemented sum function

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** argv){
+  int num1, num2;
+  cout << "Insert the first number: ";
+  cin >> num1;
+  cout << "Insert the second number: ";
+  cin >> num2;
+  cout << "Sum: " << num1 + num2 << endl; 
+}
