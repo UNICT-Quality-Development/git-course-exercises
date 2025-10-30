eeb527002f027e6fc096b6c5aca078dfa13591d6
Matti <mattiasidoti07@gmail.com>
2025-10-30 17:41:21 +0100

    feat: calculator exercise

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,27 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+
+  cout << "Insert first number: ";
+  cin >> num1;
+
+  cout << "Insert second number: ";
+  cin >> num2;
+
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  int div = num1 / num2;
+
+  cout << "\nSum: " << sum << endl;
+  cout << "Difference: " << diff << endl;
+  cout << "Multiplication: " << mul << endl;
+  cout << "Division: " << div << endl;
+}
