06ee32119f0924cdf7260172c764481f2ff66497
Thomas Morales <moraless@MoralesPC.localdomain>
2025-10-30 18:22:34 +0100

    Commit del file corretto

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,21 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+
+  int x = 0;
+
+  srand(time(NULL));
+  x = rand() % 100; 
+
+  cout << "Il numero generato in modo casuale e': " << x;
+
+  return 0;
+}
+
