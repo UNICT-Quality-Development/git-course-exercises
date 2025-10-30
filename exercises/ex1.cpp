0118226f5f428c84a9f5ade412607a5fc9cbc330
Nathh17 <ultrazocker21@gmail.com>
2025-10-30 17:40:52 +0100

    refactor: Changed if-else to switch-case

--- exercises/ex1.cpp
+++ exercises/ex1.cpp
@@ -11,37 +11,29 @@ int main()
   cout << "Enter week number(1-7): " << endl;
   cin >> week;
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
+  switch(week){
+    case 1:
+
+    break;
+    case 2:
+
+    break;
+    case 3:
+
+    break;
+    case 4:
+
+    break;
+    case 5:
+
+    break;
+    case 6:
+
+    break;
+    case 7:
+
+    break;
+
   }
 
   return 0;
