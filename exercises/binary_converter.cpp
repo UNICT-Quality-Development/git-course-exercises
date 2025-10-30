8a9f43bbe36332b6e409b57bfcb2d8f149a3d27d
Matti <mattiasidoti07@gmail.com>
2025-10-30 17:32:39 +0100

    feat: binary numbers exercise

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,24 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int num;
+  cout << "Insert number: ";
+  cin >> num;
+
+  int temp = num;
+
+  while (temp > 0)
+  {
+    cout << temp % 2;
+    temp = temp / 2;
+  }
+
+  cout << endl;
+}
