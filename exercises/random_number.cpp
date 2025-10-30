a350b781728c5350cc1ddd95162ac6039ca5aaa1
Giulio Taccetta <giulio.taccetta@gmail.com>
2025-10-30 18:36:28 +0100

    add feature random.cp

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,17 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+
+int main(){
+random_device rd;
+      mt19937 gen(rd());
+      uniform_int_distribution<> distrib(1, 500000);
+
+      cout<<distrib(gen)<<endl;
+}
+
