34f4b26a5bdd6676f7d33a3d5796acd86503ca11
Marco Scalisi <marcoscalisi2702@gamil.com>
2025-10-30 17:49:07 +0100

    update ex2

--- exercises/ex2.cpp
+++ exercises/ex2.cpp
@@ -1,51 +1,26 @@
-/*
- Improve this program replacing if/else if with an array.
-
- Hint:  arr[3] = "Thursday";
-*/
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  int week;
+    int week;
+    cout << "Enter week number (1-7): ";
+    cin >> week;
 
-  cout << "Enter week number(1-7): " << endl;
-  cin >> week;
+    // Array che contiene i nomi dei giorni
+    string days[7] = {
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    };
 
-  if (week == 1)
-  {
-    cout << "Monday" << endl;
-  }
-  else if (week == 2)
-  {
-    cout << "Tuesday" << endl;
-  }
-  else if (week == 3)
-  {
-    cout << "Wednesday" << endl;
-  }
-  else if (week == 4)
-  {
-    cout << "Thursday" << endl;
-  }
-  else if (week == 5)
-  {
-    cout << "Friday" << endl;
-  }
-  else if (week == 6)
-  {
-    cout << "Saturday" << endl;
-  }
-  else if (week == 7)
-  {
-    cout << "Sunday" << endl;
-  }
-  else
-  {
-    cout << "Invalid input! Please enter week number between 1-7." << endl;
-  }
+    if (week >= 1 && week <= 7)
+    {
+        // L'indice dell'array parte da 0, quindi usiamo week - 1
+        cout << days[week - 1] << endl;
+    }
+    else
+    {
+        cout << "Invalid input! Please enter week number between 1-7." << endl;
+    }
 
-  return 0;
+    return 0;
 }
