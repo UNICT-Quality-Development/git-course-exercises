4a47f7b4284e7ed1c6c6c74d54e88a3c27e4efd6
Filippo Marletta <filippomarletta9@gmail.com>
2025-10-30 17:30:26 +0100

    feat: Add sum program that reads two integers and prints their sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,8 +1,25 @@
 /*
   Write a program that takes as input two numbers and print the sum.
 
+
   Output:
   Insert the first number: 1
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+
+  cout << "Insert the first number: ";
+  cin >> num1;
+  cout << "Insert the second number: ";
+  cin >> num2;
+
+  cout << "Sum: " << num1 + num2 << endl;
+  return 0;
+}
