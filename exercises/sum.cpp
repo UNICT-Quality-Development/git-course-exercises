ffe4c0a4968f175029561ca5874e036654591775
VincenzoVillanova <VLLVCN02M19C351R@studium.unict.it>
2025-10-30 17:26:28 +0100

    feat: update sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,23 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+double sum(double n1, double n2)
+{
+  return n1 + n2;
+}
+
+int main()
+{
+  double num1 = 0, num2 = 0;
+  cout << "Inserire il numero 1: ";
+  cin >> num1;
+  cout << endl;
+  cout << "Inserire il numero 2: ";
+  cin >> num2;
+  cout << endl;
+
+  cout << "La somma tra " << num1 << " e " << num2 << " ammonta a " << sum(num1, num2) << endl;
+}
