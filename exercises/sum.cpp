caba2cafac4a75f73b946fe855e43c9ea655350a
enzo <enzo13617@gmail.com>
2025-10-30 17:26:41 +0100

    feat (sum.cpp): exercise done

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,21 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+	double x, y;
+	
+	cout << "Inserisci due numeri: " << endl;
+	cin >> x;
+	cin >> y;
+	double somma = x + y;
+	cout << "La somma dei due numeri è: " << somma << endl;
+
+	return 0;
+}
