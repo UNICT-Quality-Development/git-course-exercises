308ece48e9d5d93db06ac0a7aa476546f3fe2176
Igni-ss <cocoig02@gmail.com>
2025-10-30 17:23:10 +0100

    feat(sum.cpp): implemented sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Insert the first number: ";
+  int n1;
+  cin >> n1;
+  cout << "Insert the second number: ";
+  int n2;
+  cin >> n2;
+  cout << "Sum: " << n1 + n2 << endl;
+  return 0;
+}
