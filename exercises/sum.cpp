050fc18d853fbe1f6f05361fd9a1eeeaef08cd22
Piero <fiorica.gianpiero@gmail.com>
2025-10-30 17:39:27 +0100

    feature: sum

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,18 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <stdio.h>
+#include <iostream>
+
+int main(){
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	std::cout << "Inserisci un numero: \n";
+	std::cin >> a;
+	std::cout << "Inserisci un numero da sommare al primo \n";
+	std::cin >> b;
+	sum = a + b;
+	std::cout << sum <<"\n";
+	return 0;
+}
