3b7c855b85908ff2c9ff0b2f5903e8932b9077f9
enzo <enzo13617@gmail.com>
2025-11-01 10:49:33 +0100

    feat (exercise_number.cpp): exercise done

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,20 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <ctime>
+
+#define EXIT_SUCCESS 0
+
+using namespace std;
+
+int main(){
+
+  srand(time(NULL));
+  int x = rand() % 20000;
+  cout << "The random number is: "<< x <<endl;
+
+  return EXIT_SUCCESS;
+}
+
+
