85925f7ebbc1088ec56b9e27acd11b3303272c67
enzo <enzo13617@gmail.com>
2025-11-01 11:11:27 +0100

    feat (verify.cpp): exercise done

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -5,14 +5,33 @@ Output example:
 Insert number 3
 The number 3 is [not] present in the array.
 */
-
 #include <iostream>
+#define EXIT_SUCCESS 0
+#define DIM 10
+
 using namespace std;
+
+bool findNumber(int N[], int x){
+  for(int i = 0; i < DIM; i++){
+    if(x == N[i])
+      return true;
+  }
+  return false;
+}
 
 int main()
 {
-  // placeholder
-  int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int x;
+  int N[DIM] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
 
-  return 0;
+  cout << "Insert a number: " << endl;
+  cin >> x;
+  
+  bool found = findNumber(N, x);
+  if(found)
+    cout << "The number " << x << " is present in the array" << endl;
+  else
+    cout << "The number " << x << " is not present in the array" << endl;
+
+  return EXIT_SUCCESS;
 }
