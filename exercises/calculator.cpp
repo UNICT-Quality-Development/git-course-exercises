d2b622ca558985bac861b8a4de64407618490be0
Giuseppe Pio Bacchi <bacchigiuseppe21@gmail.com>
2025-11-01 08:51:37 +0100

    Implementazione esercizio calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,40 @@
   Multiplication: 8
   Division: 2
 */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    // Input dei numeri
+    cout << "Insert first number: ";
+    cin >> num1;
+    cout << "Insert second number: ";
+    cin >> num2;
+
+    // Operazioni
+    double sum = num1 + num2;
+    double difference = num1 - num2;
+    double multiplication = num1 * num2;
+    double division;
+
+    // Controllo divisione per zero
+    if(num2 != 0) {
+        division = num1 / num2;
+    } else {
+        cout << "Division by zero is not allowed." << endl;
+    }
+
+    // Output
+    cout << "SUM: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Multiplication: " << multiplication << endl;
+    if(num2 != 0) {
+        cout << "Division: " << division << endl;
+    }
+
+    return 0;
+}
