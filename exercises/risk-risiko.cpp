17f016b6f674a9d86852dc9dd6439dcbd3c5518d
Carmelo Ferrara <carmelof437@gmail.com>
2025-11-01 12:30:37 +0100

    refactor

--- exercises/risk-risiko.cpp
+++ exercises/risk-risiko.cpp
@@ -102,11 +102,11 @@ void player::roll() {
   for (int i = 1; i < 3; i++) {
     int key = a[i];
     int j = i-1;
-      while (j >= 0 && key < a[j]) {
-        a[j + 1] = a[j];
-        j--;
-      }
-      a[j + 1] = key;
+    while (j >= 0 && key < a[j]) {
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = key;
   }
   N = a[2];
   M = a[1];
