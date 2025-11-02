794b017bb0058ddbe21123dae442eb39b8414d96
Giuseppe Pio Bacchi <bacchigiuseppe21@gmail.com>
2025-11-02 23:10:50 +0100

    feat: risolto esercizio binary_converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,19 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Insert first number: ";
+    cin >> number;
+
+    cout << "The binary number is: " << bitset<32>(number) << endl;
+
+    return 0;
+}
