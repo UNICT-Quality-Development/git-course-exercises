f7ef70c4c731ce44a25547ebf1c7cd1661834239
Valeria Cutrona <Valeriacutrona07@gmail.com>
2025-11-02 21:03:38 +0100

    feat: init exercises

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -1,29 +1,12 @@
-#include<iostream>
+/*
+  Write a program that given two numbers as input make the main operations.
 
-using namespace std;
+  Output:
+  Insert first number: 4
+  Insert second number: 2
 
-int main(){
-  float num1;
-  float num2;
-
-  cout<<"Insert the first number: "<<endl;
-  cin>>num1;
-
-  cout<<"Insert the second number: "<<endl;
-  cin>>num2;
-
-  float sum,diff,mult,div;
-
-  sum = num1 + num2;
-  diff = num1 - num2;
-  mult = num1 * num2;
-  div = num1 / num2;
-
-  cout<<"SUM: "<<sum<<endl;
-  cout<<"Difference: "<<diff<<endl;
-  cout<<"Multiplication: "<<mult<<endl;
-  cout<<"Division: "<<div<<endl;
-
-  return 0;
-
-}
+  SUM: 6
+  Difference: 2
+  Multiplication: 8
+  Division: 2
+*/
