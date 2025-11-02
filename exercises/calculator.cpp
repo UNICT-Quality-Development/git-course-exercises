910fa8848f28686407162dedd5f9e9555dd869ea
Giuseppe Pio Bacchi <bacchigiuseppe21@gmail.com>
2025-11-02 11:06:34 +0100

    Correzione esercizio calculator.cpp

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -13,37 +13,42 @@
 
 
 #include <iostream>
+#include <cstdlib> // per EXIT_FAILURE
 using namespace std;
 
 int main() {
     double num1, num2;
 
-    // Input dei numeri
+    // Input del primo numero
     cout << "Insert first number: ";
-    cin >> num1;
-    cout << "Insert second number: ";
-    cin >> num2;
+    if (!(cin >> num1)) {
+        cerr << "Invalid input." << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Input del secondo numero, controllo divisione per zero
+    while (true) {
+        cout << "Inserisci secondo numero: ";
+        if (!(cin >> num2)) {
+            cerr << "ERRORE" << endl;
+            return EXIT_FAILURE;
+        }
+        if (num2 != 0) break; // numero valido
+        cout << "Inserisci un'altro numero" << endl;
+    }
 
     // Operazioni
     double sum = num1 + num2;
     double difference = num1 - num2;
     double multiplication = num1 * num2;
-    double division;
-
-    // Controllo divisione per zero
-    if(num2 != 0) {
-        division = num1 / num2;
-    } else {
-        cout << "Division by zero is not allowed." << endl;
-    }
+    double division = num1 / num2;
 
     // Output
     cout << "SUM: " << sum << endl;
     cout << "Difference: " << difference << endl;
     cout << "Multiplication: " << multiplication << endl;
-    if(num2 != 0) {
-        cout << "Division: " << division << endl;
-    }
+    cout << "Division: " << division << endl;
 
     return 0;
 }
+
