7a9cec6bbb5e75a23e353e9962407f65edab5e9f
Andrea-Giuseppe-Cavallaro <Adenor88@gmail.com>
2025-11-02 16:50:47 +0100

    feat: add function to convert integer to binary

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,44 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+using namespace std;
+
+// Funzione di conversione da intero decimale a binario
+void converter(int x, bool array[], size_t &n)
+{
+  n = 0;
+  int temp = x;
+
+  while (temp > 0)
+  {
+    temp = temp / 2;
+    n++;
+  }
+
+  size_t i = n - 1;
+  while (x > 0)
+  {
+    array[i] = x % 2;
+    x = x / 2;
+    i--;
+  }
+}
+
+int main()
+{
+  int x;
+  cout << "Insert the integer you want to convert in binary: ";
+  cin >> x;
+
+  bool bits[32]; // abbastanza grande per un int
+  size_t n;
+
+  converter(x, bits, n);
+
+  cout << "Integer:" << x << " converted in binary: ";
+  for (size_t i = 0; i < n; i++)
+    cout << bits[i];
+  cout << endl;
+}
