9a0ac2d6d6803c3b10f2f1263dff14bc6f9ffdfd
Valeria Cutrona <Valeriacutrona07@gmail.com>
2025-11-02 22:45:18 +0100

    feat: esercizio somma svolto

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,19 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Insert the first number: ";
+    cin >> a;
+
+    cout << "Insert the second number: ";
+    cin >> b;
+
+    cout << "Sum: " << a + b << endl;
+
+    return 0;
+}
