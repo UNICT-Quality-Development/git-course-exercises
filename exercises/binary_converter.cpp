ba8dd419537dc6b3fed2aaaee3e23be54148fda4
Andrea Lizzio <andrealizzio2000@gmail.com>
2025-11-02 12:30:23 +0100

    binary_converter.cpp

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,24 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+using namespace std;
+
+string convertToBinary(string x){
+  int x_int = stoi(x); 
+  while (x_int > 0){
+    string num = to_string(x_int % 2);
+    x_int /= 2; 
+    return convertToBinary(to_string(x_int)) + num;
+  }
+  return "\n";
+}
+
+int main(){
+  int x = 16;
+
+  string binary = convertToBinary(to_string(x));
+
+  cout << "Il numero: " << x << " in binario è " << binary << endl;
+}
