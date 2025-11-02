e24cfdbcfa7e26ded79c066bb0ecefb372cd9075
Valeria Cutrona <Valeriacutrona07@gmail.com>
2025-11-02 19:29:33 +0100

    feat:init exercises

---
+++ exercises/calculator.cpp
@@ -0,0 +1,12 @@
+/*
+  Write a program that given two numbers as input make the main operations.
+
+  Output:
+  Insert first number: 4
+  Insert second number: 2
+
+  SUM: 6
+  Difference: 2
+  Multiplication: 8
+  Division: 2
+*/
