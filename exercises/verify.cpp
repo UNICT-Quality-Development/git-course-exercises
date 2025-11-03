2548891afdc0a27c1a03b7a4e7db06025de85dd5
Nathh17 <ultrazocker21@gmail.com>
2025-11-03 17:56:44 +0100

    fix(verify.cpp): Fixed array name in function

--- exercises/verify.cpp
+++ exercises/verify.cpp
@@ -11,8 +11,9 @@ using namespace std;
 
 template<typename T> bool contains(const T* Arr, const size_t& dim, const T& toFind){
   for(size_t i=0; i<dim; i++)
-    if(A[i]==toFind)
+    if(Arr[i]==toFind)
       return true;
+
   return false;
 }
 
@@ -20,12 +21,13 @@ int main()
 {
   // placeholder
   int N[10] = {3, 4, 5, 1, 2, 3, 4, 9, 13, 0};
+  int toFind;
 
   cout << "Insert number to find: " << endl;
   cin >> toFind;
 
   if(contains(N, 10, toFind))
-    cout << "The number " << toFind << "is present in the array" << endl;
+    cout << "The number " << toFind << " is present in the array" << endl;
   else
     cout << "The number " << toFind << "is NOT present in the array" << endl;
 
