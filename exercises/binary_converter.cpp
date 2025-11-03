725f51a865dddf6c0e72238e14324bee18a5647e
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-04 00:36:19 +0100

    feat: implemented binary conversion

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -2,21 +2,33 @@
   Write a program that given a number as input convert it in binary.
 
   Output:
-  Insert first number: 8
+  Insert number: 8
   The binary number is: 1000
 */
 #include <iostream>
 using namespace std;
 
-unsigned int toBinary(int& num){
-  //TODO: converting function
+void printBinary(int num){
+  unsigned short arr[32]; //32 is the number of bits in a standard integer
+  size_t i;
+  for(i = 0; num > 0; i++){
+    arr[i] = num % 2; //Saving the module inside of arr[i]
+    num /= 2; 
+  }
+
+  while (i > 0){ // 'i' contains the number of bits needed for the conversion
+    cout << arr[--i];
+  }
 }
 
 int main(){
   int inputNum;
-  cout << "Insert first number: ";
+  cout << "Insert number: ";
   cin >> inputNum;
 
-  cout << "The binary number is: " << toBinary(inputNum) << endl;
+  cout << "The binary number is: ";
+  printBinary(inputNum);
+  cout << endl;
+
   return 0;
 }
