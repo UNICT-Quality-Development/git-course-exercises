35cadb6876761519fba8435146620861974f89cb
Giuseppe Tornello <giuseppe@gmail.com>
2025-11-03 12:50:15 +0100

    refactor: replaced if statement with map

--- exercises/ex3.cpp
+++ exercises/ex3.cpp
@@ -1,42 +1,34 @@
 /* Could you still use a switch case here? May you can use a map. */
 
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+unordered_map<string,string> initialize_peopleMap(){
+  unordered_map<string,string> peopleMap = {
+    {"BarackObama","44th president of the United States"},
+    {"SandroPertini","Former President of the Italian Republic"},
+    {"NelsonMandela","Former President of South Africa"},
+    {"MahatmaGandhi","Bapu"},
+    {"DonaldKnuth","Creator of LaTeX"},
+    {"DennisRitchie","Creator of C"}
+  };
+  return peopleMap;
+}
 
 int main()
 {
   string textInput;
+  unordered_map<string,string> peopleMap = initialize_peopleMap();
 
   cout << "Enter a famous name+surname, ex. BarackObama " << endl;
   cin >> textInput;
 
-  if (textInput == "BarackObama")
-  {
-    cout << "44th president of the United States" << endl;
-  }
-  else if (textInput == "SandroPertini")
-  {
-    cout << "Former President of the Italian Republic" << endl;
-  }
-  else if (textInput == "NelsonMandela")
-  {
-    cout << "Former President of South Africa" << endl;
-  }
-  else if (textInput == "MahatmaGandhi")
-  {
-    cout << "Bapu" << endl;
-  }
-  else if (textInput == "DonaldKnuth")
-  {
-    cout << "Creator of LaTeX" << endl;
-  }
-  else if (textInput == "DennisRitchie")
-  {
-    cout << "Creator of C" << endl;
-  }
-  else
-  {
+  if(peopleMap.count(textInput) > 0){
+    cout<<peopleMap[textInput]<<endl;
+  } else {
     cout << "Invalid input! Please enter a good name!" << endl;
+    return 1;
   }
 
   return 0;
