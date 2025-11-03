4f4c389b363ec2d52aa8045c6007bdf74d4969f8
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-03 23:50:54 +0100

    feat: inizialized workflow

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,18 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+using namespace std;
+
+unsigned int toBinary(int& num){
+  //TODO: converting function
+}
+
+int main(){
+  int inputNum;
+  cout << "Insert first number: ";
+  cin >> inputNum;
+
+  cout << "The binary number is: " << toBinary(inputNum) << endl;
+  return 0;
+}
