0983a72a4bdc39ebbdfed3e5bf932b1c6ce2cac3
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-04 17:32:55 +0100

    feat: implement a random number generator

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,23 @@
   Output:
   The random number is: 4
 */
+
+
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+
+int randomNumber();
+
+int main() {
+  unsigned int seed = static_cast<unsigned int>(
+    std::chrono::steady_clock::now().time_since_epoch().count()
+  );
+  std::srand(seed);
+  std::cout << "The random number is: " << randomNumber();
+  return 0;
+}
+
+int randomNumber() {
+  return std::rand() % 100;
+}
