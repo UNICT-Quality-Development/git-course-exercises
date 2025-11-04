a3d893dfe4b565ea11aad2cadb16eeedd7a4d6f3
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-04 17:11:27 +0100

    feat: implemented negative decimal conversion

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -9,11 +9,12 @@
 using namespace std;
 
 void printBinary(int num){
+  unsigned int unsigned_num = static_cast<unsigned int>(num);
   unsigned short arr[32]; //32 is the number of bits in a standard integer
   size_t i;
-  for(i = 0; num > 0; i++){
-    arr[i] = num % 2; //Saving the module inside of arr[i]
-    num /= 2; 
+  for(i = 0; unsigned_num > 0; i++){
+    arr[i] = unsigned_num % 2; //Saving the module inside of arr[i]
+    unsigned_num /= 2; 
   }
 
   while (i > 0){ // 'i' contains the number of bits needed for the conversion
