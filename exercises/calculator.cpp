1a5c0a96afc46a95f6845a64bd11700763d3c260
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-04 12:43:52 +0100

    feat: do a main operator with user input

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,74 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+#include <string>
+
+const std::string ERROR_COLOR = "\033[31m";
+const std::string NORMAL_COLOR = "\033[0m";
+
+const int ZERO = 48;
+const int NINE = 57;
+const int MAX_DIGIT = 10;
+const int MINUS = 45;
+
+int insertNumber(std::string position);
+bool checkStringIsNumber(std::string in);
+
+float doSum(float n1, float n2);
+float doSub(float n1, float n2);
+float doMolt(float n1, float n2);
+float doDiv(float n1, float n2);
+
+void doOperation(float n1, float n2);
+
+int main(){
+  float num1 = insertNumber("first");
+  float num2 = insertNumber("second");
+
+  doOperation(num1, num2);
+}
+
+
+
+int insertNumber(std::string position){
+  std::string insert;
+  bool isCorrect = true;
+  do {
+    if(!isCorrect) std::cout << ERROR_COLOR+("[Insert a valid input] ")+NORMAL_COLOR;
+    std::cout << "Insert "<< position <<" number: ";
+    getline(std::cin, insert);
+  } while(!(isCorrect = checkStringIsNumber(insert)));
+
+  return std::stoi(insert);
+}
+
+bool checkStringIsNumber(std::string in){
+  if(in.empty()) return false;
+  if(in.size() > MAX_DIGIT) return false;
+  for(int i = 0; i < in.size(); i++) 
+    if((in.at(i) < ZERO || in.at(i) > NINE) && in.at(i) != MINUS) return false;
+
+  return true;
+}
+
+float doSum(float n1, float n2) {
+  return n1+n2;
+}
+float doSub(float n1, float n2) {
+  return n1-n2;
+}
+float doMolt(float n1, float n2) {
+  return n1*n2;
+}
+float doDiv(float n1, float n2) {
+  return (n2 != 0) ? n1/n2 : -1;
+}
+
+void doOperation(float n1, float n2) {
+  std::cout << "SUM: " << doSum(n1, n2) << std::endl;
+  std::cout << "Difference: " << doSub(n1, n2) << std::endl;
+  std::cout << "Moltiplication: " << doMolt(n1, n2) << std::endl;
+  std::cout << "Division: " << doDiv(n1, n2) << std::endl;
+}
