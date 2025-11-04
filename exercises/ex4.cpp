04e4e68a96ec4d6c5dac813dbfeb64f55d2db548
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-04 17:18:11 +0100

    refactor: load old code

--- exercises/ex4.cpp
+++ exercises/ex4.cpp
@@ -1,55 +1,56 @@
 /* Surprise me. */
 
-#include <iostream>
-#include <string>
-#include <map>
+#include <stdio.h>
 
-const int ZERO = 48;
-const int NINE = 57;
-const int MAX_NUMBER = 10;
-const int MINUS = 45;
+int main()
+{
+  int month;
 
-const std::string ERROR_COLOR = "\033[31m";
-const std::string NORMAL_COLOR = "\033[0m";
+  /* Input month number from user */
+  printf("Enter month number(1-12): ");
+  scanf("%d", &month);
 
-int insertNumber();
-bool checkStringIsNumber(std::string in);
+  switch (month)
+  {
+  case 1:
+    printf("31 days");
+    break;
+  case 2:
+    printf("28/29 days");
+    break;
+  case 3:
+    printf("31 days");
+    break;
+  case 4:
+    printf("30 days");
+    break;
+  case 5:
+    printf("31 days");
+    break;
+  case 6:
+    printf("30 days");
+    break;
+  case 7:
+    printf("31 days");
+    break;
+  case 8:
+    printf("31 days");
+    break;
+  case 9:
+    printf("30 days");
+    break;
+  case 10:
+    printf("31 days");
+    break;
+  case 11:
+    printf("30 days");
+    break;
+  case 12:
+    printf("31 days");
+    break;
+  default:
+    printf("Invalid input! Please enter month number between 1-12");
+  }
 
-int main(){
-  int month = insertNumber();
-  std::map<int, std::string> calendary = {
-    {1, "31"},
-    {2, "28/29"},
-    {3, "31"},
-    {4, "30"},
-    {5, "31"},
-    {6, "30"},
-    {7, "31"},
-    {8, "31"},
-    {9, "30"},
-    {10, "31"},
-    {11, "30"},
-    {12, "31"},
-  };
-  std::cout << calendary[month] << " days";
   return 0;
-}
-
-int insertNumber(){
-  std::string insert;
-  bool isCorrect = true;
-  do {
-    if(!isCorrect) std::cout << ERROR_COLOR+("[Insert a valid input] ")+NORMAL_COLOR;
-    std::cout << "Insert Mounth: ";
-    getline(std::cin, insert);
-  } while(!(isCorrect = checkStringIsNumber(insert)));
-  return std::stoi(insert);
-}
-
-bool checkStringIsNumber(std::string in){
-  if(in.empty()) return false;
-  for(int i = 0; i < in.size(); i++)
-  if((in.at(i) < ZERO || in.at(i) > NINE) && in.at(i) != MINUS) return false;
-  if(stoi(in) > MAX_NUMBER) return false;
-  return true;
 }
