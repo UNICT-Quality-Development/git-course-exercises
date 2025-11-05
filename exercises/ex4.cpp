9c2791f417b022a993eac86f14b4cc615d4acc14
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-05 18:10:23 +0100

    refactor(ex4.cpp): add namespace, remove std:: and add more spaces

--- exercises/ex4.cpp
+++ exercises/ex4.cpp
@@ -4,20 +4,22 @@
 #include <string>
 #include <map>
 
+using namespace std;
+
 const int ZERO = 48;
 const int NINE = 57;
 const int MAX_NUMBER = 10;
 const int MINUS = 45;
 
-const std::string ERROR_COLOR = "\033[31m";
-const std::string NORMAL_COLOR = "\033[0m";
+const string ERROR_COLOR = "\033[31m";
+const string NORMAL_COLOR = "\033[0m";
 
 int insertNumber();
-bool checkStringIsNumber(std::string in);
+bool checkStringIsNumber(string in);
 
 int main(){
   int month = insertNumber();
-  std::map<int, std::string> calendary = {
+  map<int, string> calendary = {
     {1, "31"},
     {2, "28/29"},
     {3, "31"},
@@ -31,25 +33,35 @@ int main(){
     {11, "30"},
     {12, "31"},
   };
-  std::cout << calendary[month] << " days";
+  cout << calendary[month] << " days";
   return 0;
 }
 
 int insertNumber(){
-  std::string insert;
+  string insert;
   bool isCorrect = true;
+
   do {
-    if(!isCorrect) std::cout << ERROR_COLOR+("[Insert a valid input] ")+NORMAL_COLOR;
-    std::cout << "Insert Mounth: ";
-    getline(std::cin, insert);
-  } while(!(isCorrect = checkStringIsNumber(insert)));
-  return std::stoi(insert);
+    if(!isCorrect) cout << ERROR_COLOR+("[Insert a valid input] ")+NORMAL_COLOR;
+    
+    cout << "Insert Mounth: ";
+    getline(cin, insert);
+    
+    isCorrect = checkStringIsNumber(insert);
+  } while(!isCorrect);
+
+  return stoi(insert);
 }
 
-bool checkStringIsNumber(std::string in){
-  if(in.empty()) return false;
+bool checkStringIsNumber(string in){
+  if(in.empty()) 
+    return false;
+
   for(int i = 0; i < in.size(); i++)
-  if((in.at(i) < ZERO || in.at(i) > NINE) && in.at(i) != MINUS) return false;
+    if((in.at(i) < ZERO || in.at(i) > NINE) && in.at(i) != MINUS) 
+      return false;
+  
   if(stoi(in) > MAX_NUMBER) return false;
+  
   return true;
 }
