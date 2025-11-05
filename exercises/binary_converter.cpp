8bbeaecf94d4e08f91499ee0a65fbfaef1db32e2
extragit <paoloini05@gmail.com>
2025-11-05 18:01:11 +0100

    feat: Implemented Binary Converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -1,7 +1,44 @@
 /*
-  Write a program that given a number as input convert it in binary.
+    Write a program that given a number as input convert it in binary.
 
-  Output:
-  Insert first number: 8
-  The binary number is: 1000
+    Output:
+    Insert first number: 8
+    The binary number is: 1000
 */
+#include <iostream>
+
+using namespace std;
+
+void BinR(unsigned long int num){
+    if(!num){
+        return;
+    }
+
+    if(num%2){
+        BinR((num-1)/2);
+        cout<< "1";
+        return;
+    }
+
+    BinR(num/2);
+    cout<< "0";
+    return;
+}
+
+int main(){
+    unsigned long int num;
+
+    cout << "Insert first number: ";
+    cin >> num;
+
+    if(cin.fail()){
+        cerr << "Error: The given value is not a number" << endl;
+        exit(1);
+    }
+
+    cout<< "The binary number is: ";
+    BinR(num);
+
+    cout << endl;
+    return 0;
+}
