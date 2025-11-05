34feacff1c3fafca16678f7c084d8177e77af04b
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-05 18:13:17 +0100

    style(sum.cpp): remove spaces and remove std::

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -9,32 +9,27 @@
 
 #include <iostream>
 
+using namespace std;
+
 const int ZERO = 48;
 const int NINE = 57;
 const int MAX_DIGIT = 10;
 const int MINUS = 45;
 
-const std::string ERROR_COLOR = "\033[31m";
-const std::string NORMAL_COLOR = "\033[0m";
-
-// template <typename T>
-// T sumNumber(T n1, T n2);
-
-// auto insertNumber(const std::string& msg);
-// bool checkStringIsNumber(std::string in);
+const string ERROR_COLOR = "\033[31m";
+const string NORMAL_COLOR = "\033[0m";
 
 template <typename T>
 T sumNumber(T n1, T n2);
-double insertNumber(const std::string& msg);
-bool checkStringIsNumber(std::string in);
+double insertNumber(const string& msg);
+bool checkStringIsNumber(string in);
 
 
 int main() {
   auto n1 = insertNumber("Insert first number:");
   auto n2 = insertNumber("Insert second number:");
-  
   auto ris = sumNumber(n1, n2);
-  std::cout << "Sum: " << ris;
+  cout << "Sum: " << ris;
 }
 
 template <typename T>
@@ -42,22 +37,22 @@ T sumNumber(T n1, T n2) {
   return n1 + n2;
 }
 
-double insertNumber(const std::string& msg){
-  std::string insert;
+double insertNumber(const string& msg){
+  string insert;
   bool isCorrect = true;
   do {
-    if(!isCorrect) std::cout << ERROR_COLOR << "[Insert a valid input] " << NORMAL_COLOR;
-    std::cout << msg;
-    getline(std::cin, insert);
+    if(!isCorrect) cout << ERROR_COLOR << "[Insert a valid input] " << NORMAL_COLOR;
+    cout << msg;
+    getline(cin, insert);
   } while(!(isCorrect = checkStringIsNumber(insert)));
 
-  return std::stod(insert);
+  return stod(insert);
 }
 
-bool checkStringIsNumber(std::string in){
+bool checkStringIsNumber(string in){
   if(in.empty()) return false;
   if(in.size() > MAX_DIGIT) return false;
-  for (std::size_t i = 0; i < in.size(); ++i){
+  for (size_t i = 0; i < in.size(); ++i){
     if ((in[i] < ZERO || in[i] > NINE)){
       if (i == 0 && in[i] == MINUS) continue;
       return false;
