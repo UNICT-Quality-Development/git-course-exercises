bbda40e0fa660abf65599efeb692d8adea6efe29
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-05 18:02:37 +0100

    style(ex3.cpp): remove whitespaces

--- exercises/ex3.cpp
+++ exercises/ex3.cpp
@@ -8,13 +8,13 @@ int main()
 {
   string textInput;
 
-  map<string, string> famDes = { 
-    {"BarackObama", "44th president of the United States"}, 
-    {"SandroPertini", "Former President of the Italian Republic"}, 
-    {"NelsonMandela", "Former President of South Africa"}, 
+  map<string, string> famDes = {
+    {"BarackObama", "44th president of the United States"},
+    {"SandroPertini", "Former President of the Italian Republic"},
+    {"NelsonMandela", "Former President of South Africa"},
     {"MahatmaGandhi", "Bapu"},
     {"DonaldKnuth", "Creator of LaTeX"},
-    {"DennisRitchie", "Creator of C"} };
+    {"DennisRitchie", "Creator of C"}};
 
   cout << "Enter a famous name+surname, ex. BarackObama " << endl;
   cin >> textInput;
