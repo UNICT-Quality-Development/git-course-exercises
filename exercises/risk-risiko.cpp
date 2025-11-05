4bef88414535d588f4f2e2483cd56508dbd28afa
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-05 17:48:05 +0100

    feat: implement risk risiko battle with dices

--- exercises/risk-risiko.cpp
+++ exercises/risk-risiko.cpp
@@ -27,3 +27,62 @@
   M 3 vs 3 => blue win
   O 2 vs 1 => red win
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <chrono>
+#include <algorithm>
+
+using namespace std;
+
+const int NUM_DICES_FOR_USER = 3;
+const short int RED_WIN = 1;
+const short int BLUE_WIN = -1;
+const unsigned short int FACES_DICES = 6;
+
+unsigned int * generateDicesNumber();
+void compareValue(unsigned int * dices);
+void showDices(unsigned int * dices);
+
+int main() {
+  unsigned int seed = static_cast<unsigned int>(
+    chrono::steady_clock::now().time_since_epoch().count()
+  );
+  srand(seed);
+
+  unsigned int * dices = generateDicesNumber();
+  
+  sort(dices, dices+NUM_DICES_FOR_USER);
+  sort(dices+NUM_DICES_FOR_USER, dices+(NUM_DICES_FOR_USER*2));
+  
+  showDices(dices);
+  compareValue(dices);
+
+  free(dices);
+}
+
+unsigned int * generateDicesNumber(){
+  unsigned int * dices = new unsigned int;
+  for(int i = 0; i < NUM_DICES_FOR_USER*2; i++) dices[i] = rand() % FACES_DICES + 1; 
+  return dices;
+}
+
+void compareValue(unsigned int * dices) {
+  cout << endl << "  R    B";
+
+  for(int i = NUM_DICES_FOR_USER-1; i >= 0; i--) {
+    cout << endl << "N " << dices[i] << " vs " << dices[i+NUM_DICES_FOR_USER];
+    if(dices[i] > dices[i+NUM_DICES_FOR_USER]) cout << " => RED WIN";
+    else cout << " => BLUE WIN";
+  }
+  cout << endl;
+}
+
+void showDices(unsigned int * dices){
+  cout << "\nBlue dices: " << endl;
+  for(int i = NUM_DICES_FOR_USER*2-1; i >= 0; i--) {
+    if(i == 2) cout << "\n\nRed dices: " << endl;
+    cout << dices[i] << endl;
+  }
+}
