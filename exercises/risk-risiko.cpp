9a716a0c0c8ba8285d896242266f657bd93fd26f
antonioialazzo <endoni@DESKTOP-T7JEQF4.localdomain>
2025-11-11 15:19:05 +0100

    feat: solve risk-risiko.cpp

--- exercises/risk-risiko.cpp
+++ exercises/risk-risiko.cpp
@@ -27,3 +27,36 @@
   M 3 vs 3 => blue win
   O 2 vs 1 => red win
 */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+void throwDices(int *v, size_t n){
+  for (int i=0; i<n; i++){
+    v[i] = (rand() % 6) + 1;
+  }
+  sort(v, v+n, greater<int>());
+}
+
+int main() {
+  srand(time(nullptr));
+  int v1[3],v2[3];
+  char chs[3]= {'N', 'M', 'O'};
+  throwDices(v1, 3);
+  throwDices(v2, 3);
+  cout << "Red dices: " << endl;
+  for (int i=0; i<3; i++){
+    cout << v1[i] << " (" << chs[i] << ")"<< endl;
+  }
+  cout << "\nBlue dices: " << endl;
+  for (int i=0; i<3; i++){
+    cout << v2[i] << " (" << chs[i] << ")"<< endl;
+  }
+
+  printf("\n%3.c%5.c\n", 'R', 'B');
+  for (int i=0; i<3; i++){
+  cout << chs[i] << " " << v1[i] << " vs " << v2[i] << " => " << (v1[i]>v2[i] ? "red win" : "blue win" ) << endl;
+  }
+}
